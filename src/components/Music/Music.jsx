eed0f8524ab6af61c3857c88d9e9bfb5e6d410e4
Andrey <andrey.slesarchuk@gmail.com>
2019-05-19 22:25:35 +0300

    change Music albums

--- src/components/Music/Music.jsx
+++ src/components/Music/Music.jsx
@@ -15,11 +15,15 @@ const  Music = (props) => {
                 <div className={c.album}>
                     DIO
                 </div>
+                <div className={c.album}>
+                    Aria
+                </div>
             </div>
             <div className={c.songs}>
                 <div className={c.song}>Metal heart</div>
                 <div className={c.song}>Nothing else matter</div>
                 <div className={c.song}>Seven keys of seven sons</div>
+                <div className={c.song}>Will and mind</div>
             </div>
         </div>
 
