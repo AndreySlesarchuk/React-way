6692110cfb45f3138b57b9eae5865f8e9dfd1795
Andrey Slesarchuk <andrey.slesarchuk@gmail.com>
2019-05-21 22:58:55 +0300

    change Dialogs order

--- src/components/Dialogs/Dialogs.jsx
+++ src/components/Dialogs/Dialogs.jsx
@@ -7,12 +7,12 @@ let dialogs = [
     {id:1, name: 'Tana'},
     {id:2, name: 'Masha'},
     {id:3, name: 'Mike'},
-    {id:4, name: 'Andrey'},
-    {id:5, name: 'Vasiliy'}
+    {id:4, name: 'Vasily'},
+    {id:5, name: 'Andrey'}
 ]
 
 let messages = [
-    {id:1, message: 'Hi'},
+    {id:1, message: 'Hi all'},
     {id:2, message: 'Hello World!'},
     {id:3, message: 'How is your react-way?'},
     {id:4, message: 'Yup'},
