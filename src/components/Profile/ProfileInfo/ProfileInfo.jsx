03373e384701b35fb2f8605b1266142ab094b1af
Andrey Slesarchuk <andrey.slesarchuk@gmail.com>
2019-05-24 11:40:02 +0300

    changed images for profile

--- src/components/Profile/ProfileInfo/ProfileInfo.jsx
+++ src/components/Profile/ProfileInfo/ProfileInfo.jsx
@@ -1,11 +1,12 @@
 import React from 'react';
 import c from './ProfileInfo.module.css'
+//<img src='https://cdn-images-1.medium.com/max/1600/1*CvjclMDwgh9AqED_h5UVCA.jpeg'/>
 
 const ProfileInfo = () => {
     return (
         <div>
             <div className={c.content}>
-                <img src='https://cdn-images-1.medium.com/max/1600/1*CvjclMDwgh9AqED_h5UVCA.jpeg'/>
+                <img src='https://cdnb.artstation.com/p/assets/images/images/005/754/755/large/jayson-kassis-highresscreenshot00199.jpg?1493552247'/>
             </div>
             <div className={c.descriptionBlock}>
                 ava + description
