f4ce04af16f6869ac9c71b5bdf3227d28c72ac04
Andrey Slesarchuk <andrey.slesarchuk@gmail.com>
2019-05-28 19:29:08 +0300

    atmm.txt lightshot
    snipping tool - lightshot

--- src/components/Profile/MyPosts/Post/Post.jsx
+++ src/components/Profile/MyPosts/Post/Post.jsx
@@ -3,7 +3,6 @@ import React from 'react';
 import c from './Post.module.css'
 
 const Post = (props) => {
-    debugger;
     return (
         <div className={c.item}>
             <img className={c.img} src='https://blog.eu.playstation.com/files/avatars/avatar_467916.jpg' />
