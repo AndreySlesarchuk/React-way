4f5b30dcd7cfc822557288811b5fe92081e99700
Andrey Slesarchuk <andrey.slesarchuk@gmail.com>
2019-05-28 19:14:26 +0300

    migration state
    index.js render.js state.js conception

--- src/render.js
+++ src/render.js
@@ -2,10 +2,10 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
-import state, {addPost} from './redux/state';
+import {addPost} from './redux/state';
 import {BrowserRouter} from "react-router-dom";
 
-export let rerenderEntireTree = () => {
+export let rerenderEntireTree = (state) => {
     ReactDOM.render(
         <BrowserRouter>
             <App state={state} addPost={addPost}/>
