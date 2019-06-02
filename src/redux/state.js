31407aeccd2acbe9f9e29d551f6aa4889da270dd
Andrey Slesarchuk <andrey.slesarchuk@gmail.com>
2019-06-02 22:41:45 +0300

    Update state.js
    dispatch 3

--- src/redux/state.js
+++ src/redux/state.js
@@ -42,23 +42,7 @@ let store = {
     subscribe(observer) {
         this._callSubscriber = observer; //observer
     },
-
-    addPost() {
-        debugger;
-        let newPost = {
-            id: 5,
-            message: this._state.profilePage.newPostText,
-            likesCount: 0
-        };
-        this._state.profilePage.posts.push(newPost);
-        this._state.profilePage.newPostText = '';
-        this._callSubscriber(this._state);
-    },
-    updateNewPostText(newText) {
-        this._state.profilePage.newPostText = newText;
-        this._callSubscriber(this._state);
-    },
-
+    
     dispatch(action) { // { type: 'ADD-POST' }
         if (action.type === 'ADD-POST') {
             let newPost = {
@@ -75,6 +59,7 @@ let store = {
         }
     }
 }
+
 export default store;
 window.store = store;
 //store = OOP
