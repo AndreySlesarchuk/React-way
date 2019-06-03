8054dd0ffe3df1657206d0c42cdda2987030e6dc
Andrey Slesarchuk <andrey.slesarchuk@gmail.com>
2019-06-03 13:46:43 +0300

    Update state.js
    added constants

--- src/redux/state.js
+++ src/redux/state.js
@@ -1,3 +1,8 @@
+const ADD_POST = 'ADD-POST';
+const UPDATE_NEW_POST_TEXT = 'UPDATE_NEW_POST_TEXT';
+
+
+
 let store = {
     _state: {
         profilePage: {
@@ -44,7 +49,7 @@ let store = {
     },
 
     dispatch(action) { // { type: 'ADD-POST' }
-        if (action.type === 'ADD-POST') {
+        if (action.type === ADD_POST) {
             let newPost = {
                 id: 5,
                 message: this._state.profilePage.newPostText,
@@ -53,7 +58,7 @@ let store = {
             this._state.profilePage.posts.push(newPost);
             this._state.profilePage.newPostText = '';
             this._callSubscriber(this._state);
-        } else if (action.type === 'UPDATE-NEW-POST-TEXT') {
+        } else if (action.type === UPDATE_NEW_POST_TEXT) {
             this._state.profilePage.newPostText = action.newText;
             this._callSubscriber(this._state);
         }
@@ -61,16 +66,16 @@ let store = {
 }
 
 export const addPostActionCreator = () => {
+
     return {
-        type: 'ADD-POST'
+        type: ADD_POST
     }
 }
 export const updateNewPostActionCreator = (text) => {
     return {
-        type: 'UPDATE-NEW-POST-TEXT', newText: text
+        type: UPDATE_NEW_POST_TEXT
     }
 }
-
 
 export default store;
 window.store = store;
