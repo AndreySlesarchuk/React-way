222af1579f8587c72d3138428b8db7731422462f
Andrey Slesarchuk <andrey.slesarchuk@gmail.com>
2019-06-04 21:17:46 +0300

    Update MyMap.jsx
    refactoring

--- src/components/MyMap/MyMap.jsx
+++ src/components/MyMap/MyMap.jsx
@@ -1,32 +1,20 @@
-const React = window.React;
-const { Map, TileLayer, Marker, Popup } = window.ReactLeaflet;
+import {Map, Marker, TileLayer} from 'react-leaflet';
 
-class SimpleExample extends React.Component {
-    constructor() {
-        super();
-        this.state = {
-            lat: 51.505,
-            lng: -0.09,
-            zoom: 13,
-        };
-    }
 
-    render() {
-        const position = [this.state.lat, this.state.lng];
-        return (
-            <Map center={position} zoom={this.state.zoom}>
-                <TileLayer
-                    attribution='&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
-                    url='http://{s}.tile.osm.org/{z}/{x}/{y}.png'
-                />
-                <Marker position={position}>
-                    <Popup>
-                        <span>A pretty CSS3 popup. <br/> Easily customizable.</span>
-                    </Popup>
-                </Marker>
-            </Map>
-        );
-    }
+const MyMap = () => {
+    return (
+       <Map className="markercluster-map" center={[51.0, 19.0]} zoom={4} maxZoom={18}>
+        <TileLayer
+            url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+            attribution='&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
+        />
+
+        {/*<MarkerClusterGroup>
+        <Marker position={[49.8397, 24.0297]} />
+        <Marker position={[52.2297, 21.0122]} />
+        <Marker position={[51.5074, -0.0901]} />
+    </MarkerClusterGroup>*/}
+
+    </Map>
+    )
 }
-export default SimpleExample;
-window.ReactDOM.render(<SimpleExample />, document.getElementById('container'));
