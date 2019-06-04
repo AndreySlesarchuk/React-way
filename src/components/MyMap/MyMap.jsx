e78a4fccc19f33c8822e2f6b781900bd8858ae1f
Andrey Slesarchuk <andrey.slesarchuk@gmail.com>
2019-06-04 12:53:05 +0300

    Update MyMap.jsx
    small fix

--- src/components/MyMap/MyMap.jsx
+++ src/components/MyMap/MyMap.jsx
@@ -2,24 +2,27 @@ import React from "react";
 import {Marker, Popup, TileLayer} from 'react-leaflet'
 
 const MyMap = () => {
-    const position = [53.9, 27.5];
-    const positionPinsk = [52.12, 26.09];
+    let position = [53.9, 27.5];
+    let positionPinsk = [52.12, 26.09];
+    debugger;
     return (
-        <MyMap center={position} zoom={7}>
-            <TileLayer
-                attribution='&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
-                url='https://{s}.tile.osm.org/{z}/{x}/{y}.png'
-            />
-            <Marker position={positionPinsk}>
+        <div>
+            <MyMap center={position} zoom={7}>
+                <TileLayer
+                    attribution='&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
+                    url='https://{s}.tile.osm.org/{z}/{x}/{y}.png'
+                />
+                <Marker position={positionPinsk}>
 
 
-                <Popup>
-                    <img src='https://my-calend.ru/data/source/pinsk-gerb.png' />
+                    <Popup>
+                        <img src='https://my-calend.ru/data/source/pinsk-gerb.png'/>
 
-                    A pretty CSS3 popup with Pinsk logo. <br/> Easily customizable.
-                </Popup>
-            </Marker>
-        </MyMap>
+                        A pretty CSS3 popup with Pinsk logo. <br/> Easily customizable.
+                    </Popup>
+                </Marker>
+            </MyMap>
+        </div>
     )
 }
 
