cb016370e91631f64dd95a61ac37970391e8b489
Andrey Slesarchuk <andrey.slesarchuk@gmail.com>
2019-06-05 15:58:44 +0300

    added open street map
    added open street map

--- src/components/Geomap/Geomap.js
+++ src/components/Geomap/Geomap.js
@@ -3,12 +3,20 @@ import L from 'leaflet';
 
 class Geomap extends React.Component {
     componentDidMount() {
-        this.map = L.map(this.ref['mount']).setView([52.12, 26.09], 7);
+        this.map = L.map(this.refs['mount']).setView([52.12, 26.09], 7);
+        L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+            attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a>'
+        }).addTo(this.map);
     }
 
     render() {
+        const {className, style} = this.props;
         return (
-            <div ref='mount' />
+            <div>
+
+            <div ref='mount' className={className} style={style}/>
+
+            </div>
         )
     }
 }
