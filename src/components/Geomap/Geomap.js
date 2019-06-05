0ee82717ed0ca49561adf915eb6d2a3e0180e1eb
Andrey Slesarchuk <andrey.slesarchuk@gmail.com>
2019-06-05 16:19:23 +0300

    working Geomap
    added Geomap with belarus

--- src/components/Geomap/Geomap.js
+++ src/components/Geomap/Geomap.js
@@ -1,22 +1,25 @@
 import React from 'react';
 import L from 'leaflet';
+import 'leaflet/dist/leaflet.css'
 
 class Geomap extends React.Component {
     componentDidMount() {
-        this.map = L.map(this.refs['mount']).setView([52.12, 26.09], 7);
+        this.map = L.map(this.refs['mount']).setView([53.9, 27.5], 7);
         L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
             attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a>'
         }).addTo(this.map);
     }
 
     render() {
+        const positionPinsk = [52.12, 26.09];
         const {className, style} = this.props;
         return (
-            <div>
 
-            <div ref='mount' className={className} style={style}/>
+            <div ref='mount' className={className} style={style} />
 
-            </div>
+
+
+
         )
     }
 }
