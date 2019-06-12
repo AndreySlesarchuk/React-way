ea05cec6c6dd397958abf2105f43b7d9842bef1a
Andrey Slesarchuk <andrey.slesarchuk@gmail.com>
2019-06-12 22:26:56 +0300

    Update state.js

--- src/redux/state.js
+++ src/redux/state.js
@@ -9,7 +9,7 @@ let store = {
             posts: [
                 {id: 1, message: 'Study good my friend', likesCount: 13},
                 {id: 2, message: 'Hello my world!', likesCount: 7},
-                {id: 3, message: 'Good morning', likesCount: 11},
+                {id: 3, message: 'Good morning!', likesCount: 11},
                 {id: 4, message: 'Yup yup yup', likesCount: 8}
             ],
             newPostText: 'atmm.by'
