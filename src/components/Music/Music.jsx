caed3a20ce9563833ce107b7dc85576a3df1f96a
Andrey Slesarchuk <andrey.slesarchuk@gmail.com>
2019-06-17 22:00:12 +0300

    Update Music.jsx

--- src/components/Music/Music.jsx
+++ src/components/Music/Music.jsx
@@ -10,7 +10,7 @@ const  Music = (props) => {
                     Highland
                 </div>
                 <div className={c.album}>
-                    Metallica
+                    Elza's ocean
                 </div>
                 <div className={c.album}>
                     Black Coffee
@@ -21,7 +21,7 @@ const  Music = (props) => {
             </div>
             <div className={c.songs}>
                 <div className={c.song}>Solo Tu</div>
-                <div className={c.song}>Nothing else matter</div>
+                <div className={c.song}>Autumn</div>
                 <div className={c.song}>Wooden churches of Russia</div>
                 <div className={c.song}>Will and mind</div>
             </div>
