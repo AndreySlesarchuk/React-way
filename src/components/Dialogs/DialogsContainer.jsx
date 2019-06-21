f3387ba7fb006be37acf5850ce2939095ea67dc7
Andrey Slesarchuk <andrey.slesarchuk@gmail.com>
2019-06-21 16:05:02 +0300

    Data from Context

--- src/components/Dialogs/DialogsContainer.jsx
+++ src/components/Dialogs/DialogsContainer.jsx
@@ -1,25 +1,30 @@
 import React from "react";
 import {sendMessageCreator, updateNewMessageBodyCreator} from "../../redux/dialogs-reducer";
 import Dialogs from "./Dialogs";
+import StoreContext from "../../StoreContext"
 
 
-const DialogsContainer = (props) => {
+const DialogsContainer = () => {
 
-    let state = props.store.getState();
+    return <StoreContext.Consumer>
+        {
+        (store) => {
+            let state = store.getState();
 
-    let onSendMessageClick = () => {
-        props.store.dispatch(sendMessageCreator());
+            let onSendMessageClick = () => {
+                store.dispatch(sendMessageCreator());
+            }
+            let onNewMessageChange = (body) => {
+                store.dispatch(updateNewMessageBodyCreator(body));
+            }
+            return <Dialogs updateNewMessageBody={onNewMessageChange}
+                            sendMessage={onSendMessageClick}
+                            dialogs={state.dialogsPage.dialogs}
+                            messages={state.dialogsPage.messages}
+                            newMessageBoby={state.dialogsPage.newMessageBody}/>
+        }
     }
-    let onNewMessageChange = (body) => {
-        props.store.dispatch(updateNewMessageBodyCreator(body));
-    }
-
-    return <Dialogs updateNewMessageBody={onNewMessageChange}
-                    sendMessage={onSendMessageClick}
-                    dialogs={state.dialogsPage.dialogs}
-                    messages={state.dialogsPage.messages}
-                    newMessageBoby={state.dialogsPage.newMessageBody}
-    />
+    </StoreContext.Consumer>
 }
 
 export default DialogsContainer;
--- src/components/Profile/MyPosts/MyPostsContainer.jsx
+++ src/components/Profile/MyPosts/MyPostsContainer.jsx
@@ -3,29 +3,27 @@ import {addPostActionCreator, updateNewPostActionCreator} from "../../../redux/p
 import MyPosts from "./MyPosts";
 import StoreContext from "../../../StoreContext"
 
-const MyPostsContainer = (props) => {
+const MyPostsContainer = () => {
 
-    return (
-        <StoreContext.Consumer> {
-            (store) => {
-
-                let state = props.store.getState();
-                let addPost = () => {
-                    props.store.dispatch(addPostActionCreator());
-                }
-                let onPostChange = (text) => {
-                    let action = updateNewPostActionCreator(text);
-                    props.store.dispatch(action);
-                }
-
-                return <MyPosts updateNewPostText={onPostChange}
-                                addPost={addPost}
-                                posts={state.profilePage.posts}
-                                newPostText={state.profilePage.newPostText}/>
+    return <StoreContext.Consumer>
+        {
+        (store) => {
+            let state = store.getState();
+            let addPost = () => {
+                store.dispatch(addPostActionCreator());
             }
+            let onPostChange = (text) => {
+                let action = updateNewPostActionCreator(text);
+                store.dispatch(action);
+            }
+
+            return <MyPosts updateNewPostText={onPostChange}
+                            addPost={addPost}
+                            posts={state.profilePage.posts}
+                            newPostText={state.profilePage.newPostText}/>
         }
-        </StoreContext.Consumer>
-    )
+    }
+    </StoreContext.Consumer>
 }
 
 export default MyPostsContainer;
