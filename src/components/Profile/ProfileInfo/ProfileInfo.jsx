44ca2d20992603426653256dd1670663306420fe
Andrey Slesarchuk <andrey.slesarchuk@gmail.com>
2019-06-22 22:39:20 +0300

    new image for Profile

--- src/components/Profile/ProfileInfo/ProfileInfo.jsx
+++ src/components/Profile/ProfileInfo/ProfileInfo.jsx
@@ -1,12 +1,12 @@
 import React from 'react';
 import c from './ProfileInfo.module.css'
-//<img src='https://cdn-images-1.medium.com/max/1600/1*CvjclMDwgh9AqED_h5UVCA.jpeg'/>
 
+//<img src='https://cdnb.artstation.com/p/assets/images/images/005/754/755/large/jayson-kassis-highresscreenshot00199.jpg?1493552247'/>
 const ProfileInfo = () => {
     return (
         <div>
             <div className={c.content}>
-                <img src='https://cdnb.artstation.com/p/assets/images/images/005/754/755/large/jayson-kassis-highresscreenshot00199.jpg?1493552247'/>
+                <img src='https://cdn-images-1.medium.com/max/1600/1*CvjclMDwgh9AqED_h5UVCA.jpeg'/>
             </div>
             <div className={c.descriptionBlock}>
                 ava + description
