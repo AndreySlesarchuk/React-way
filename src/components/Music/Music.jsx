f4949005402aca05aa11854c45da2ec52eb4a900
Andrey Slesarchuk <andrey.slesarchuk@gmail.com>
2019-06-25 22:16:05 +0300

    Update Music.jsx

--- src/components/Music/Music.jsx
+++ src/components/Music/Music.jsx
@@ -7,7 +7,7 @@ const  Music = (props) => {
         <div className={c.music}>
             <div className={c.musicAlbums}>
                 <div className={c.album}>
-                    Highland
+                    Armin van Buren
                 </div>
                 <div className={c.album}>
                     UDO
@@ -23,7 +23,7 @@ const  Music = (props) => {
                 </div>
             </div>
             <div className={c.songs}>
-                <div className={c.song}>Solo to</div>
+                <div className={c.song}>Kazantip</div>
                 <div className={c.song}>Crying soldier</div>
                 <div className={c.song}>Kill the pain</div>
                 <div className={c.song}>Princess of the down</div>
