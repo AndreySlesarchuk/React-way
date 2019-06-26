ad9e72188c55ff931e5ec6134fff236e1cc4dfc7
Andrey Slesarchuk <andrey.slesarchuk@gmail.com>
2019-06-26 22:36:56 +0300

    Update Music.jsx

--- src/components/Music/Music.jsx
+++ src/components/Music/Music.jsx
@@ -10,7 +10,7 @@ const  Music = (props) => {
                     Armin van Buren
                 </div>
                 <div className={c.album}>
-                    Black coffee
+                    Aria
                 </div>
                 <div className={c.album}>
                     Helloween
@@ -24,7 +24,7 @@ const  Music = (props) => {
             </div>
             <div className={c.songs}>
                 <div className={c.song}>Kazantip</div>
-                <div className={c.song}>Wooden church</div>
+                <div className={c.song}>will and mind</div>
                 <div className={c.song}>Kill the pain</div>
                 <div className={c.song}>Princess of the down</div>
                 <div className={c.song}>Kings of metal</div>
