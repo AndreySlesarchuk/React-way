3b5815167a727c6ebd6e6735eb44937a4a3edc08
Andrey Slesarchuk <andrey.slesarchuk@gmail.com>
2019-06-29 18:09:04 +0300

    added Users

--- src/App.js
+++ src/App.js
@@ -20,8 +20,13 @@ const App = () => {
             <Header/>
             <Navbar/>
             <div className='app-wrapper-content'>
-                <Route path='/profile' render={() => <Profile /> }/>
-                <Route path='/dialogs' render={() => <DialogsContainer /> }/>
+                <Route path='/profile'
+                       render={() => <Profile /> }/>
+                <Route path='/dialogs'
+                       render={() => <DialogsContainer /> }/>
+                <Route path='/users'
+                       render={() => <div>users</div>} />
+
                 <Route path='/news' render={() => <News />} />
                 <Route path='/music' render={() => <Music />} />
                 <Route path='/todo' render={() => <Todo />} />
