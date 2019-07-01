41ec5246ffad80e94894dd52ae66a2964e8e6aa3
Andrey Slesarchuk <andrey.slesarchuk@gmail.com>
2019-07-01 16:38:46 +0300

    Update users-reducer.js
    add SET_USER

--- src/redux/users-reducer.js
+++ src/redux/users-reducer.js
@@ -1,29 +1,12 @@
 const FOLLOW = 'FOLLOW';
 const UNFOLLOW = 'UNFOLLOW';
+const SET_USERS = 'SET_USERS';
 
 let initialState = {
     users: [
-        {
-            id: 1,
-            followed: true,
-            fullname: 'Andrey',
-            status: 'I am a boss',
-            location: {city: 'Minsk', country: 'Belarus'}
-        },
-        {
-            id: 2,
-            followed: false,
-            fullname: 'Bogdan',
-            status: 'I am a boss',
-            location: {city: 'Kiev', country: 'Ukraine'}
-        },
-        {
-            id: 3,
-            followed: true,
-            fullname: 'Valya',
-            status: 'I am a boss',
-            location: {city: 'Moskow', country: 'Russia'}
-        },
+        {id: 1, followed: true, fullname: 'Andrey', status: 'I am a boss', location: {city: 'Minsk', country: 'Belarus'}},
+        {id: 2, followed: false, fullname: 'Bogdan', status: 'I am a boss', location: {city: 'Kiev', country: 'Ukraine'}},
+        {id: 3, followed: true, fullname: 'Valya', status: 'I am a boss', location: {city: 'Moskow', country: 'Russia'}},
         {id: 4, followed: true, fullname: 'Mike', status: 'I am a boss', location: {city: 'Pinsk', country: 'Belarus'}},
     ]
 }
@@ -51,13 +34,16 @@ const usersReducer = (state = initialState, action) => {
                     return u;
                 })
             }
-
+        case SET_USERS: {
+            return { ...state, users: [...state.users, ...action.users]}
+        }
         default:
             return state;
     }
 }
 // if only returns value
-export const followActionCreator = () => ({type: FOLLOW, userId})
-export const unfollowActionCreator = () => ({type: UNFOLLOW, userId})
+export const followActionCreator = (userId) => ({type: FOLLOW, userId})
+export const unfollowActionCreator = (userId) => ({type: UNFOLLOW, userId})
+export const setUsersActionCreator = (users) => ({type: SET_USERS, users})
 
 export default usersReducer;
