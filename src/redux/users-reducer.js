ca4fea1e465e8c7e87d9fda1a1b94f3b81dc7949
Andrey Slesarchuk <andrey.slesarchuk@gmail.com>
2019-07-01 14:07:27 +0300

    Update users-reducer.js
    added action followed unfollowed

--- src/redux/users-reducer.js
+++ src/redux/users-reducer.js
@@ -1,27 +1,63 @@
 const FOLLOW = 'FOLLOW';
 const UNFOLLOW = 'UNFOLLOW';
-const UPDATE_NEW_POST_TEXT = 'UPDATE_NEW_POST_TEXT';
 
 let initialState = {
     users: [
-        {id: 1, followed: true, fullname: 'Andrey', status: 'I am a boss', location: {city: 'Minsk', country:'Belarus'}},
-        {id: 2, followed: false, fullname: 'Bogdan', status: 'I am a boss', location: {city: 'Kiev', country:'Ukraine'}},
-        {id: 3, followed: true, fullname: 'Valya', status: 'I am a boss', location: {city: 'Moskow', country:'Russia'}},
-        {id: 4, followed: true, fullname: 'Mike', status: 'I am a boss', location: {city: 'Pinsk', country:'Belarus'}},
+        {
+            id: 1,
+            followed: true,
+            fullname: 'Andrey',
+            status: 'I am a boss',
+            location: {city: 'Minsk', country: 'Belarus'}
+        },
+        {
+            id: 2,
+            followed: false,
+            fullname: 'Bogdan',
+            status: 'I am a boss',
+            location: {city: 'Kiev', country: 'Ukraine'}
+        },
+        {
+            id: 3,
+            followed: true,
+            fullname: 'Valya',
+            status: 'I am a boss',
+            location: {city: 'Moskow', country: 'Russia'}
+        },
+        {id: 4, followed: true, fullname: 'Mike', status: 'I am a boss', location: {city: 'Pinsk', country: 'Belarus'}},
     ]
 }
 
 
 const usersReducer = (state = initialState, action) => {
     switch (action.type) {
+        case FOLLOW:
+            return {
+                ...state,
+                users: state.users.map(u => {
+                    if (u.id === action.userId) {
+                        return {...u, followed: true}
+                    }
+                    return u;
+                })
+            }
+        case UNFOLLOW:
+            return {
+                ...state,
+                users: state.users.map(u => {
+                    if (u.id === action.userId) {
+                        return {...u, followed: false}
+                    }
+                    return u;
+                })
+            }
+
         default:
             return state;
     }
 }
 // if only returns value
-export const followActionCreator = () => ({type: FOLLOW})
-export const unfollowActionCreator = () => ({type: UNFOLLOW})
-export const updateNewPostActionCreator = (text) => {
-    return {type: UPDATE_NEW_POST_TEXT, newText: text}
-}
+export const followActionCreator = () => ({type: FOLLOW, userId})
+export const unfollowActionCreator = () => ({type: UNFOLLOW, userId})
+
 export default usersReducer;
