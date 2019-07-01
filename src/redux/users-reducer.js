4cbb96406179239311cde82bbd0e13b3386534c1
Andrey Slesarchuk <andrey.slesarchuk@gmail.com>
2019-07-01 13:34:33 +0300

    Update users-reducer.js
    added followed

--- src/redux/users-reducer.js
+++ src/redux/users-reducer.js
@@ -3,10 +3,10 @@ const UPDATE_NEW_POST_TEXT = 'UPDATE_NEW_POST_TEXT';
 
 let initialState = {
     users: [
-        {id: 1, fullname: 'Andrey', status: 'I am a boss', location: {city: 'Minsk', country:'Belarus'}},
-        {id: 2, fullname: 'Bogdan', status: 'I am a boss', location: {city: 'Kiev', country:'Ukraine'}},
-        {id: 3, fullname: 'Valya', status: 'I am a boss', location: {city: 'Moskow', country:'Russia'}},
-        {id: 4, fullname: 'Mike', status: 'I am a boss', location: {city: 'Pinsk', country:'Belarus'}},
+        {id: 1, followed: true, fullname: 'Andrey', status: 'I am a boss', location: {city: 'Minsk', country:'Belarus'}},
+        {id: 2, followed: false, fullname: 'Bogdan', status: 'I am a boss', location: {city: 'Kiev', country:'Ukraine'}},
+        {id: 3, followed: true, fullname: 'Valya', status: 'I am a boss', location: {city: 'Moskow', country:'Russia'}},
+        {id: 4, followed: true, fullname: 'Mike', status: 'I am a boss', location: {city: 'Pinsk', country:'Belarus'}},
     ]
 }
 
