6da65c5824fc2b2d81e202e0f114861e97a13f20
Andrey Slesarchuk <andrey.slesarchuk@gmail.com>
2019-07-02 18:10:37 +0300

    userPhoto default

--- src/components/Users/Users.jsx
+++ src/components/Users/Users.jsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import styles from './Users.module.css';
 import * as axios from "axios";
+import userPhoto from '../../assets/images/user.png';
 
 let Users = (props) => {
     if (props.users.length === 0) {
@@ -49,7 +50,7 @@ let Users = (props) => {
             props.users.map(u => <div key={u.id}>
                 <span>
                     <div>
-                        <img src={u.photoUrl} className={styles.userPhoto}/>
+                        <img src={u.photos.small != null ? u.photos.small : userPhoto} className={styles.userPhoto}/>
                     </div>
                     <div>
                         {u.followed
