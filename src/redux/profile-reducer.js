f471e7c2c6edf77ea77b77457a13a70596cc91b3
Andrey Slesarchuk <andrey.slesarchuk@gmail.com>
2019-07-03 21:21:20 +0300

    Update profile-reducer.js

--- src/redux/profile-reducer.js
+++ src/redux/profile-reducer.js
@@ -6,7 +6,7 @@ let initialState = {
         {id: 1, message: 'Hello!', likesCount: 13},
         {id: 2, message: 'Good day', likesCount: 8},
         {id: 3, message: 'Victory day', likesCount: 7},
-        {id: 4, message: 'Good night', likesCount: 11}
+        {id: 4, message: 'summer evening', likesCount: 11}
     ],
     newPostText: 'www.atmm.ru'
 }
