bbe06ce764f8b7db02c9a5868b0e05739c56bfd9
Andrey Slesarchuk <andrey.slesarchuk@gmail.com>
2019-07-04 16:37:17 +0300

    button Get Users

--- src/components/Users/Users.jsx
+++ src/components/Users/Users.jsx
@@ -4,49 +4,52 @@ import * as axios from "axios";
 import userPhoto from '../../assets/images/user.png';
 
 let Users = (props) => {
-    if (props.users.length === 0) {
 
-        axios.get("https://social-network.samuraijs.com/api/1.0/users").
-        then(response => {
-            props.setUsers(response.data.items);
-        });
+    let getUsers = () => {
+        if (props.users.length === 0) {
 
-        /*props.setUsers([
-                {
-                    id: 1,
-                    photoUrl: 'https://scontent-frx5-1.xx.fbcdn.net/v/t1.0-1/p160x160/20375969_2123357487891108_7861845126847512805_n.jpg?_nc_cat=111&_nc_oc=AQm-AdUM7bQpERJlKFfUaP9WZrAK3mZZq5ErrhgJFCeT6NnijdBGHQk240fLZbDvbO8&_nc_ht=scontent-frx5-1.xx&oh=7f0e401ff816a57acae0a6421ac8f2d9&oe=5DAF13D6',
-                    followed: true,
-                    fullname: 'Andrey',
-                    status: 'I am a boss',
-                    location: {city: 'Minsk', country: 'Belarus'}
-                },
-                {
-                    id: 2,
-                    photoUrl: 'https://scontent-frx5-1.xx.fbcdn.net/v/t1.0-1/p160x160/20375969_2123357487891108_7861845126847512805_n.jpg?_nc_cat=111&_nc_oc=AQm-AdUM7bQpERJlKFfUaP9WZrAK3mZZq5ErrhgJFCeT6NnijdBGHQk240fLZbDvbO8&_nc_ht=scontent-frx5-1.xx&oh=7f0e401ff816a57acae0a6421ac8f2d9&oe=5DAF13D6',
-                    followed: false,
-                    fullname: 'Bogdan',
-                    status: 'I am a boss',
-                    location: {city: 'Kiev', country: 'Ukraine'}
-                },
-                {
-                    id: 3,
-                    photoUrl: 'https://pp.userapi.com/vcYmvWu65RQR8ThH4H4onUQTCDkPQH3--Ma9Iw/gJkvd0aS6Jg.jpg?ava=1',
-                    followed: true,
-                    fullname: 'Valya',
-                    status: 'I am a boss',
-                    location: {city: 'Moskow', country: 'Russia'}
-                },
-                {
-                    id: 4,
-                    photoUrl: 'https://pp.userapi.com/c629115/v629115031/45724/HIYYl-pdsfA.jpg?ava=1',
-                    followed: true,
-                    fullname: 'Mike',
-                    status: 'I am a boss',
-                    location: {city: 'Pinsk', country: 'Belarus'}
-                }
-            ])*/
+            axios.get("https://social-network.samuraijs.com/api/1.0/users").then(response => {
+                props.setUsers(response.data.items);
+            });
+
+            /*props.setUsers([
+                        {
+                            id: 1,
+                            photoUrl: 'https://scontent-frx5-1.xx.fbcdn.net/v/t1.0-1/p160x160/20375969_2123357487891108_7861845126847512805_n.jpg?_nc_cat=111&_nc_oc=AQm-AdUM7bQpERJlKFfUaP9WZrAK3mZZq5ErrhgJFCeT6NnijdBGHQk240fLZbDvbO8&_nc_ht=scontent-frx5-1.xx&oh=7f0e401ff816a57acae0a6421ac8f2d9&oe=5DAF13D6',
+                            followed: true,
+                            fullname: 'Andrey',
+                            status: 'I am a boss',
+                            location: {city: 'Minsk', country: 'Belarus'}
+                        },
+                        {
+                            id: 2,
+                            photoUrl: 'https://scontent-frx5-1.xx.fbcdn.net/v/t1.0-1/p160x160/20375969_2123357487891108_7861845126847512805_n.jpg?_nc_cat=111&_nc_oc=AQm-AdUM7bQpERJlKFfUaP9WZrAK3mZZq5ErrhgJFCeT6NnijdBGHQk240fLZbDvbO8&_nc_ht=scontent-frx5-1.xx&oh=7f0e401ff816a57acae0a6421ac8f2d9&oe=5DAF13D6',
+                            followed: false,
+                            fullname: 'Bogdan',
+                            status: 'I am a boss',
+                            location: {city: 'Kiev', country: 'Ukraine'}
+                        },
+                        {
+                            id: 3,
+                            photoUrl: 'https://pp.userapi.com/vcYmvWu65RQR8ThH4H4onUQTCDkPQH3--Ma9Iw/gJkvd0aS6Jg.jpg?ava=1',
+                            followed: true,
+                            fullname: 'Valya',
+                            status: 'I am a boss',
+                            location: {city: 'Moskow', country: 'Russia'}
+                        },
+                        {
+                            id: 4,
+                            photoUrl: 'https://pp.userapi.com/c629115/v629115031/45724/HIYYl-pdsfA.jpg?ava=1',
+                            followed: true,
+                            fullname: 'Mike',
+                            status: 'I am a boss',
+                            location: {city: 'Pinsk', country: 'Belarus'}
+                        }
+                    ])*/
+        }
     }
     return <div>
+        <button onClick={getUsers}>Get Users</button>
         {
             props.users.map(u => <div key={u.id}>
                 <span>
