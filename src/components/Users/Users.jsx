0f3ad242e7f6d95033dc766757781bfc4d783fa8
Andrey Slesarchuk <andrey.slesarchuk@gmail.com>
2019-07-06 18:47:35 +0300

    currentPage users from server

--- src/components/Users/Users.jsx
+++ src/components/Users/Users.jsx
@@ -12,6 +12,14 @@ class Users extends React.Component {
         });
     }
 
+    onPageChanged = (pageNumber) => {
+        this.props.setCurrentPage(pageNumber);
+        axios.get(`https://social-network.samuraijs.com/api/1.0/users?page=${pageNumber}&count=${this.props.pageSize}`)
+            .then(response => {
+                this.props.setUsers(response.data.items);
+            });
+    }
+
     render() {
 
         let pagesCount = Math.ceil(this.props.totalUsersCount / this.props.pageSize);
@@ -25,7 +33,7 @@ class Users extends React.Component {
             <div>
                 {pages.map (p => {
                     return <span className = {this.props.currentPage === p && styles.selectedPage}
-                    onClick={()=>{this.props.setCurrentPage(p)}}>{p}</span>
+                    onClick={(e)=>{this.onPageChanged(p); }}>{p}</span>
                 })}
             </div>
             {
