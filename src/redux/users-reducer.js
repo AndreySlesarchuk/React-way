691f4537181a62207a195b9bd488a8481fa814a0
Andrey Slesarchuk <andrey.slesarchuk@gmail.com>
2019-07-09 19:13:33 +0300

    Update users-reducer.js

--- src/redux/users-reducer.js
+++ src/redux/users-reducer.js
@@ -3,6 +3,7 @@ const UNFOLLOW = 'UNFOLLOW';
 const SET_USERS = 'SET_USERS';
 const SET_CURRENT_PAGE = 'SET_CURRENT_PAGE';
 const SET_TOTAL_USERS_COUNT = 'SET_TOTAL_USERS_COUNT';
+const TOGGLE_IS_FETCHING = 'TOGGLE_IS_FETCHING';
 
 let initialState = {
     users:[],
@@ -43,15 +44,18 @@ const usersReducer = (state = initialState, action) => {
         case SET_TOTAL_USERS_COUNT: {
             return { ...state, totalUsersCount: action.count}
         }
+        case TOGGLE_IS_FETCHING: {
+            return { ...state, isFetching: action.isFetching}
+        }
         default:
             return state;
     }
 }
-// if only returns value
+// AC - ActionCreator
 export const followActionCreator = (userId) => ({type: FOLLOW, userId})
 export const unfollowActionCreator = (userId) => ({type: UNFOLLOW, userId})
 export const setUsersActionCreator = (users) => ({type: SET_USERS, users})
 export const setCurrentPageActionCreator = (currentPage) => ({type: SET_CURRENT_PAGE, currentPage})
 export const setUsersTotalCountActionCreator = (totalUsersCount) =>({type: SET_TOTAL_USERS_COUNT, count:totalUsersCount})
-
+export const toggleIsFetchingAC = (isFetching) =>({type: TOGGLE_IS_FETCHING, isFetching})
 export default usersReducer;
