9af5ffa39abe30d4d725bb351401efba5b860117
Andrey Slesarchuk <andrey.slesarchuk@gmail.com>
2019-07-13 18:24:39 +0300

    Update ProfileContainer.jsx

--- src/components/Profile/ProfileContainer.jsx
+++ src/components/Profile/ProfileContainer.jsx
@@ -1,7 +1,18 @@
 import * as React from "react";
 import Profile from "./Profile";
+import * as axios from "axios";
 
 class ProfileContainer extends React.Component {
+
+    componentDidMount = () => {
+        axios.get(`https://social-network.samuraijs.com/api/1.0/profile/2`)
+            .then(response => {
+                this.props.toggleIsFetching(false);
+                this.props.setUsers(response.data.items);
+                this.props.setTotalUsersCount(response.data.totalCount);
+            });
+    }
+
     render() {
         return (
             <div>
