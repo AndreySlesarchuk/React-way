eec46b19f9a4ed854d23f2c5d836c1dcbbb9a307
Andrey Slesarchuk <andrey.slesarchuk@gmail.com>
2019-07-14 17:52:33 +0300

    Update ProfileContainer.jsx

--- src/components/Profile/ProfileContainer.jsx
+++ src/components/Profile/ProfileContainer.jsx
@@ -16,7 +16,7 @@ class ProfileContainer extends React.Component {
     render() {
         return (
             <div>
-                <Profile {...this.props} />
+                <Profile {...this.props} profile={this.props.profile} />
             </div>
         )
     }
