0d9b14e49b57bc6b6824cf5c0d8381659ecdfb05
Andrey Slesarchuk <andrey.slesarchuk@gmail.com>
2019-07-14 18:02:47 +0300

    added avatar and preloader to Profile

--- src/components/Profile/ProfileInfo/ProfileInfo.jsx
+++ src/components/Profile/ProfileInfo/ProfileInfo.jsx
@@ -1,14 +1,19 @@
 import React from 'react';
 import c from './ProfileInfo.module.css'
+import Preloader from "../../common/Preloader/Preloader";
 
 //<img src='https://cdnb.artstation.com/p/assets/images/images/005/754/755/large/jayson-kassis-highresscreenshot00199.jpg?1493552247'/>
-const ProfileInfo = () => {
+const ProfileInfo = (props) => {
+    if (!props.profile) {
+        return <Preloader/>
+    }
     return (
         <div>
             <div className={c.content}>
                 <img src='https://cdn-images-1.medium.com/max/1600/1*CvjclMDwgh9AqED_h5UVCA.jpeg'/>
             </div>
             <div className={c.descriptionBlock}>
+                <img src={props.profile.photos.large} />
                 ava + description
             </div>
         </div>
