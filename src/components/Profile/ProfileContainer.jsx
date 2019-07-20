9215f9a1c28e28dd7ac20733f4ae245a69110053
Andrey Slesarchuk <andrey.slesarchuk@gmail.com>
2019-07-20 23:01:49 +0300

    add WithUrlDataContainerComponent

--- src/components/Profile/ProfileContainer.jsx
+++ src/components/Profile/ProfileContainer.jsx
@@ -3,6 +3,7 @@ import {connect} from "react-redux";
 import Profile from "./Profile";
 import * as axios from "axios";
 import {setUserProfile} from "../../redux/profile-reducer";
+import {withRouter} from "react-router-dom";
 
 class ProfileContainer extends React.Component {
 
@@ -28,5 +29,6 @@ let mapStateToProps = (state) => {
     }
 }
 
+let WithUrlDataContainerComponent = withRouter(ProfileContainer);
 
-export default connect(mapStateToProps, {setUserProfile})(ProfileContainer);
+export default connect(mapStateToProps, {setUserProfile})(WithUrlDataContainerComponent);
