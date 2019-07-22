4c44dca778c9ef45472da5f55793f1d8dabcf2ab
AndreySlesarchuk <andrey.slesarchuk@gmail.com>
2019-07-22 22:52:03 +0300

    add login

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,10 +1,14 @@
 import React from 'react';
-import c from './Header.module.css'
+import style from './Header.module.css'
+import {NavLink} from "react-router-dom";
 
 const Header = () => {
-    return  <header className={c.header}>
+    return  <header className={style.header}>
         <img className='App-logo' src='http://atmm.ru/623/custom/themes/default/images/company_logo.png?v=NyHHMP7OnQElMG2V_-oPog' />
-        {/*<img className='App-logo' src='./src/images/atmm_logo.jpg' />*/}
+
+        <div className={style.loginBlock}>
+            <NavLink to={'/login'}>Login</NavLink>
+        </div>
     </header>
 }
 
--- src/redux/auth-reducer.js
+++ src/redux/auth-reducer.js
@@ -17,6 +17,6 @@ const authReducer = (state = initialState, action) => {
             return state;
     }
 }
-// ActionCreator -> AC -> none (followAC -> follow)
+
 export const setUserDats = (userId, email, login) => ({type: SET_USER_DATA, data: {userId, email, login}})
 export default authReducer;
