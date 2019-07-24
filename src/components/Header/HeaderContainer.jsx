2ecd441c40b592c938344feaafadb97f2f9798cb
Andrey Slesarchuk <andrey.slesarchuk@gmail.com>
2019-07-24 21:53:46 +0300

    Update HeaderContainer.jsx

--- src/components/Header/HeaderContainer.jsx
+++ src/components/Header/HeaderContainer.jsx
@@ -1,15 +1,12 @@
 import React from 'react';
 import style from './Header.module.css'
 import {NavLink} from "react-router-dom";
+import Header from "./Header";
 
-const Header = () => {
-    return  <header className={style.header}>
-        <img className='App-logo' src='http://atmm.ru/623/custom/themes/default/images/company_logo.png?v=NyHHMP7OnQElMG2V_-oPog' />
-
-        <div className={style.loginBlock}>
-            <NavLink to={'/login'}>Login</NavLink>
-        </div>
-    </header>
+class HeaderContainer extends React.Component {
+    render() {
+        return <Header {...this.props}/>
+    }
 }
 
-export default Header;
+export default HeaderContainer;
