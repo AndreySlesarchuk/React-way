cfc037ef3f6d712b78bd96303d59381dcaeacaac
Andrey Slesarchuk <andrey.slesarchuk@gmail.com>
2019-07-29 23:49:08 +0300

    props to Header

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -2,12 +2,13 @@ import React from 'react';
 import style from './Header.module.css'
 import {NavLink} from "react-router-dom";
 
-const Header = () => {
+const Header = (props) => {
     return  <header className={style.header}>
         <img className='App-logo' src='http://atmm.ru/623/custom/themes/default/images/company_logo.png?v=NyHHMP7OnQElMG2V_-oPog' />
 
         <div className={style.loginBlock}>
-            <NavLink to={'/login'}>Login</NavLink>
+            { props.isAuth ? props.login :
+            <NavLink to={'/login'}>Login</NavLink> }
         </div>
     </header>
 }
--- src/components/Header/HeaderContainer.jsx
+++ src/components/Header/HeaderContainer.jsx
@@ -20,5 +20,8 @@ class HeaderContainer extends React.Component {
         return <Header {...this.props}/>
     }
 }
-const mapStateToProps = (state) => ({});
+const mapStateToProps = (state) => ({
+    isAuth: state.auth.isAuth,
+    login: state.auth.login,
+});
 export default connect(mapStateToProps, {setAuthUserData}) (HeaderContainer);
