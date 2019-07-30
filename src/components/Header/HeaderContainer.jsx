6e3052c59cb04581c2dd330274365e0cdac84e87
Andrey Slesarchuk <andrey.slesarchuk@gmail.com>
2019-07-30 16:35:52 +0300

    Update HeaderContainer.jsx

--- src/components/Header/HeaderContainer.jsx
+++ src/components/Header/HeaderContainer.jsx
@@ -9,8 +9,8 @@ class HeaderContainer extends React.Component {
         axios.get(`https://social-network.samuraijs.com/api/1.0/auth/me`, {withCredentials: true})
             .then(response => {
                 if  (response.data.resultCode === 0) {
-                    let {id, email, login} = response.data.data;
-                    this.props.setAuthUserData(id, email, login);
+                    //let {id, email, login} = response.data.data;
+                    this.props.setAuthUserData(response.data.data);
                 }
                 debugger;
             });
