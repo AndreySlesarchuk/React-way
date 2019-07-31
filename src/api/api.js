99bb136a36891baa2ed5a6eca27632962daf51f7
Andrey Slesarchuk <andrey.slesarchuk@gmail.com>
2019-07-31 16:35:05 +0300

    added instance

--- src/api/api.js
+++ src/api/api.js
@@ -1,12 +1,22 @@
 import * as axios from "axios";
 
-const baseUrl = `https://social-network.samuraijs.com/api/1.0`;
+const instance = axios.create({
+    withCredentials: true,
+    baseURL: `https://social-network.samuraijs.com/api/1.0/`,
+    headers: {
+        "API-KEY": "02b51016-fda9-413b-96e5-32697749202c"
+    }
+});
 
 export const getUsers = (currentPage = 1, pageSize = 10) => {
-    return axios.get(baseUrl + `/users?page=${currentPage}&count=${pageSize}`,
-        {withCredentials: true})
+    return instance.get(`users?page=${currentPage}&count=${pageSize}`)
         .then(response => response.data);
         //.then(response => {
         //    return response.data;
         //});
+}
+
+export const getUsers2 = (currentPage = 1, pageSize = 10) => {
+    return instance.get(`follow?page=${currentPage}&count=${pageSize}`)
+        .then(response => response.data);
 }
