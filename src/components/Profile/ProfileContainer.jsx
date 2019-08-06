eb80f00c61ee8d405ebc1432b349133e3b7fbc8e
Andrey Slesarchuk <andrey.slesarchuk@gmail.com>
2019-08-06 15:09:18 +0300

    redirect from profile

--- src/components/Profile/ProfileContainer.jsx
+++ src/components/Profile/ProfileContainer.jsx
@@ -2,7 +2,7 @@ import * as React from "react";
 import {connect} from "react-redux";
 import Profile from "./Profile";
 import {getUserProfile} from "../../redux/profile-reducer";
-import {withRouter} from "react-router-dom";
+import {Redirect, withRouter} from "react-router-dom";
 
 class ProfileContainer extends React.Component {
 
@@ -13,6 +13,8 @@ class ProfileContainer extends React.Component {
     }
 
     render() {
+        if (this.props.isAuth == false) return <Redirect to={"/login"} />;
+
         return (
             <div>
                 <Profile {...this.props} profile={this.props.profile} />
@@ -23,7 +25,8 @@ class ProfileContainer extends React.Component {
 
 let mapStateToProps = (state) => {
     return {
-        profile: state.profilePage.profile
+        profile: state.profilePage.profile,
+        isAuth: state.auth.isAuth
     }
 }
 
