ae9a1772f8b4c5b9697d3a05b9e430b38609d487
AndreySlesarchuk <andrey.slesarchuk@gmail.com>
2019-08-10 12:03:40 +0300

    add state.auth.isAuth to hoc

--- src/components/Profile/ProfileContainer.jsx
+++ src/components/Profile/ProfileContainer.jsx
@@ -2,7 +2,7 @@ import * as React from "react";
 import {connect} from "react-redux";
 import Profile from "./Profile";
 import {getUserProfile} from "../../redux/profile-reducer";
-import {Redirect, withRouter} from "react-router-dom";
+import {withRouter} from "react-router-dom";
 import {withAuthRedirect} from "../../hoc/withAuthRedirect";
 
 class ProfileContainer extends React.Component {
@@ -27,14 +27,8 @@ class ProfileContainer extends React.Component {
 
 let AuthRedirectComponent = withAuthRedirect(ProfileContainer);
 
-let mapStateToPropsForRedirect = (state) => ({
-        isAuth: state.auth.isAuth
-});
-AuthRedirectComponent = connect(mapStateToPropsForRedirect)(AuthRedirectComponent);
-
-
 let mapStateToProps = (state) => ({
-            profile: state.profilePage.profile,
+    profile: state.profilePage.profile
 });
 
 let WithUrlDataContainerComponent = withRouter(AuthRedirectComponent);
