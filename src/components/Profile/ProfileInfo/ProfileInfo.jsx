354a143524852b6a2e377436a706e9b48bcd5b72
AndreySlesarchuk <andrey.slesarchuk@gmail.com>
2019-08-12 22:50:29 +0300

    add ProfileState

--- src/components/Profile/ProfileInfo/ProfileInfo.jsx
+++ src/components/Profile/ProfileInfo/ProfileInfo.jsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import c from './ProfileInfo.module.css'
 import Preloader from "../../common/Preloader/Preloader";
+import ProfileStatus from "./ProfileStatus"
 import backgroundImage from "../../../assets/images/bg_cells.jpg";
 
 //<img src='https://cdnb.artstation.com/p/assets/images/images/005/754/755/large/jayson-kassis-highresscreenshot00199.jpg?1493552247'/>
@@ -17,7 +18,7 @@ const ProfileInfo = (props) => {
             </div>*/}
             <div className={c.descriptionBlock}>
                 <img src={props.profile.photos.large} />
-                ava + description
+                <ProfileStatus/>
             </div>
         </div>
     )
---
+++ src/components/Profile/ProfileInfo/ProfileStatus.jsx
@@ -0,0 +1,16 @@
+import React from 'react';
+import c from './ProfileInfo.module.css'
+
+const ProfileStatus = (props) => {
+    return (
+        <>
+        <div>
+                <span>{props.status}</span>
+        </div>
+            <div>
+                <span>{props.status}</span>
+            </div>
+    )
+}
+
+export default ProfileStatus;
