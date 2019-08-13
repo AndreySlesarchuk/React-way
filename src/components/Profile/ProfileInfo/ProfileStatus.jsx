77420a90f872c7f422a3df0f1fae9f4f70ea2b0a
Andrey Slesarchuk <andrey.slesarchuk@gmail.com>
2019-08-13 13:15:32 +0300

    this.forceUpdate();

--- src/components/Profile/ProfileInfo/ProfileStatus.jsx
+++ src/components/Profile/ProfileInfo/ProfileStatus.jsx
@@ -3,7 +3,12 @@ import React from 'react';
 class ProfileStatus extends React.Component {
 
     state = {
-        editMode: true
+        editMode: false
+    }
+
+    activateEditMode () {
+        this.state.editMode = true;
+        this.forceUpdate();
     }
 
     render() {
@@ -11,12 +16,12 @@ class ProfileStatus extends React.Component {
             <div>
                 {!this.state.editMode &&
                     <div>
-                        <span onDoubleClick={()=>{alert('hey')}}>{this.props.status}</span>
+                        <span onDoubleClick={ this.activateEditMode.bind(this) }>{this.props.status}</span>
                     </div>
                 }
                 {this.state.editMode &&
                     <div>
-                        <input onDoubleClick={()=>{alert('hey')}} value={this.props.status}/>
+                        <input onDoubleClick={ this.activateEditMode.bind(this) } value={this.props.status}/>
                     </div>
                 }
             </div>
