a1ff58fd75fd94bc689ee44dec75414b4c4a6aae
Andrey Slesarchuk <andrey.slesarchuk@gmail.com>
2019-08-16 13:28:32 +0300

    Update api.js

--- src/api/api.js
+++ src/api/api.js
@@ -26,6 +26,12 @@ export const usersAPI = {
 export const profileAPI = {
     getProfile(userId) {
         return instance.get(`profile/${userId}`);
+    },
+    getStatus(userId) {
+        return instance.get(`status/` + userId);
+    },
+    updateStatus(status) {
+        return instance.put(`status`);
     }
 }
 
