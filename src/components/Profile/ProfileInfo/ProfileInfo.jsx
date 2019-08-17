5e8f75d146e6368a4fee20d8eadf80a09fe3903e
Andrey Slesarchuk <andrey.slesarchuk@gmail.com>
2019-08-17 18:41:04 +0300

    Update ProfileInfo.jsx

--- src/components/Profile/ProfileInfo/ProfileInfo.jsx
+++ src/components/Profile/ProfileInfo/ProfileInfo.jsx
@@ -18,7 +18,7 @@ const ProfileInfo = (props) => {
             </div>*/}
             <div className={c.descriptionBlock}>
                 <img src={props.profile.photos.large} />
-                <ProfileStatus status={props.status}/>
+                <ProfileStatus status={props.status} updateStatus={props.updateStatus} />
             </div>
         </div>
     )
