f655ad052deb70bcdc325c215dd31ea0e960434d
Andrey Slesarchuk <andrey.slesarchuk@gmail.com>
2019-08-17 18:13:05 +0300

    Update ProfileStatus.jsx

--- src/components/Profile/ProfileInfo/ProfileStatus.jsx
+++ src/components/Profile/ProfileInfo/ProfileStatus.jsx
@@ -9,29 +9,27 @@ class ProfileStatus extends React.Component {
     activateEditMode = () => {
         this.setState({
             editMode: true
-        })
+        });
         /*this.state.editMode = true;
         this.forceUpdate();*/
     }
 
-    deactivateEditMode () {
-        this.setState({
-            editMode: false
-        })
+    deactivateEditMode = () => {
+        this.setState({editMode: false});
     }
 
     render() {
         return (
             <div>
                 {!this.state.editMode &&
-                    <div>
-                        <span onDoubleClick={ this.activateEditMode }>{this.props.status}</span>
-                    </div>
+                <div>
+                    <span onDoubleClick={this.activateEditMode}>{this.props.status}</span>
+                </div>
                 }
                 {this.state.editMode &&
-                    <div>
-                        <input autoFocus={true} onBlur={ this.deactivateEditMode.bind(this) } value={this.props.status}/>
-                    </div>
+                <div>
+                    <input autoFocus={true} onBlur={this.deactivateEditMode} value={this.props.status}/>
+                </div>
                 }
             </div>
         )
