d27c8e3d206d7d23dd9dc4db03ae74f99e2d7e00
AndreySlesarchuk <andrey.slesarchuk@gmail.com>
2019-08-20 22:15:35 +0300

    Update Music.jsx

--- src/components/Music/Music.jsx
+++ src/components/Music/Music.jsx
@@ -16,7 +16,7 @@ const  Music = (props) => {
                     ARIA
                 </div>
                 <div className={c.album}>
-                    Accept
+                    ATMM
                 </div>
                 <div className={c.album}>
                     UDO
@@ -26,7 +26,7 @@ const  Music = (props) => {
                 <div className={c.song}>Night</div>
                 <div className={c.song}>Nothing else matters</div>
                 <div className={c.song}>Hero of asphalt</div>
-                <div className={c.song}>Russian winter</div>
+                <div className={c.song}>atmm</div>
                 <div className={c.song}>Crying soldier</div>
             </div>
         </div>
