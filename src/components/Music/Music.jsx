67d73f6ee030870597a8fb3cf9a296cd2ce25fd8
AndreySlesarchuk <andrey.slesarchuk@gmail.com>
2019-09-03 21:34:23 +0300

    Update Music.jsx

--- src/components/Music/Music.jsx
+++ src/components/Music/Music.jsx
@@ -23,7 +23,7 @@ const  Music = (props) => {
                 </div>
             </div>
             <div className={c.songs}>
-                <div className={c.song}>Tale</div>
+                <div className={c.song}>Night</div>
                 <div className={c.song}>Птица На Подоконнике</div>
                 <div className={c.song}>Princess ot the Dawn</div>
                 <div className={c.song}>Kill the pain</div>
