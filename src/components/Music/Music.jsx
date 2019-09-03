e3c391958bea5de880455ee5dbeb503255b82b4d
AndreySlesarchuk <andrey.slesarchuk@gmail.com>
2019-09-03 21:37:27 +0300

    Update Music.jsx

--- src/components/Music/Music.jsx
+++ src/components/Music/Music.jsx
@@ -10,9 +10,6 @@ const  Music = (props) => {
                     KINO
                 </div>
                 <div className={c.album}>
-                    BI-2
-                </div>
-                <div className={c.album}>
                     UDO
                 </div>
                 <div className={c.album}>
@@ -21,13 +18,16 @@ const  Music = (props) => {
                 <div className={c.album}>
                     Iron Maiden
                 </div>
+                <div className={c.album}>
+                    BI-2
+                </div>
             </div>
             <div className={c.songs}>
                 <div className={c.song}>Night</div>
-                <div className={c.song}>Птица На Подоконнике</div>
                 <div className={c.song}>Princess ot the Dawn</div>
                 <div className={c.song}>Kill the pain</div>
                 <div className={c.song}>Seven keys for the seven sons</div>
+                <div className={c.song}>Птица На Подоконнике</div>
             </div>
         </div>
 
