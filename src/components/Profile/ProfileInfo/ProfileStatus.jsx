67224d71a7ce0958a4db0712498256d21245ef23
Andrey Slesarchuk <andrey.slesarchuk@gmail.com>
2019-09-05 19:08:58 +0300

    Update ProfileStatus.jsx

--- src/components/Profile/ProfileInfo/ProfileStatus.jsx
+++ src/components/Profile/ProfileInfo/ProfileStatus.jsx
@@ -29,9 +29,12 @@ class ProfileStatus extends React.Component {
     }
 
     componentDidUpdate(prevProps, prevState) {
-        debugger;
-        let a = this.state;
-        let b = this.props;
+        if (prevProps.status !== this.props.status) {
+            this.setState({
+                status: this.props.status
+            })
+        }
+        //debugger;
         console.log("componentDidUpdate")
     }
 
