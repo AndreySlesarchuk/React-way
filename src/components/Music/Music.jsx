e8a9280d7b8d112f31d194b0f668cb5d3e8d9aef
Andrey Slesarchuk <andrey.slesarchuk@gmail.com>
2019-09-06 21:02:22 +0300

    Update Music.jsx

--- src/components/Music/Music.jsx
+++ src/components/Music/Music.jsx
@@ -24,7 +24,7 @@ const  Music = (props) => {
             </div>
             <div className={c.songs}>
                 <div className={c.song}>Legenda</div>
-                <div className={c.song}>Kill the pain</div>
+                <div className={c.song}>Russian rulets</div>
                 <div className={c.song}>Wooden churchs</div>
                 <div className={c.song}>Нажми на кнопку</div>
                 <div className={c.song}>Фиолетово черный</div>
