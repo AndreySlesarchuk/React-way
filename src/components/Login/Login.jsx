cbac1d8f73ea64015a2435f6ae6bdf3be0f00926
Andrey Slesarchuk <andrey.slesarchuk@gmail.com>
2019-09-07 18:48:33 +0300

    Update Login.jsx
    add LoginReduxForm

--- src/components/Login/Login.jsx
+++ src/components/Login/Login.jsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import {reduxForm} from "redux-form";
 
 
 const LoginForm = (props) => {
@@ -20,10 +21,14 @@ const LoginForm = (props) => {
     )
 }
 
+const LoginReduxForm = reduxForm({
+    form: 'login'
+}) (LoginForm)
+
 const Login = (props) => {
     return <div>
         <h1>Login</h1>
-        <LoginForm />
+        <LoginReduxForm />
     </div>
 }
 
