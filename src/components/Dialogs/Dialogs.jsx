a3bf6469b7349a9ffd9c18f3e8c6a6eb1680af06
AndreySlesarchuk <andrey.slesarchuk@gmail.com>
2019-09-12 22:09:01 +0300

    Update Dialogs.jsx

--- src/components/Dialogs/Dialogs.jsx
+++ src/components/Dialogs/Dialogs.jsx
@@ -10,9 +10,9 @@ const Dialogs = (props) => {
 
     let dialogsElements = state.dialogs.map(d => <DialogItem name={d.name} key={d.id} id={d.id}/>);
     let messagesElements = state.messages.map(m => <Message message={m.message} key={m.id}/>);
- //   let newMessageBody = state.newMessageBody;
+    let newMessageBody = state.newMessageBody;
 
-//    let onSendMessageClick = () => {
+    let onSendMessageClick = () => {
         props.sendMessage();
     }
     let onNewMessageChange = (e) => {
