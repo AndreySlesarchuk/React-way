14b184fb4d205d107c97f431a02d7ea04cba8366
AndreySlesarchuk <andrey.slesarchuk@gmail.com>
2019-09-13 22:40:09 +0300

    Update Preloader.js

--- src/components/common/Preloader/Preloader.js
+++ src/components/common/Preloader/Preloader.js
@@ -2,7 +2,6 @@ import React from "react";
 import preloader from "../../../assets/images/Pacman-1s-200px.svg";
 
 let Preloader = () => {
-    //return <div style={{backgroundColor: 'white'}}>
     return <div>
         <img src={preloader} alt=""/>
     </div>
