659113d2093c013f31d3b159d106d80ade003435
Andrey Slesarchuk <andrey.slesarchuk@gmail.com>
2019-09-28 18:39:17 +0300

    added length validators

--- src/components/Profile/MyPosts/MyPosts.jsx
+++ src/components/Profile/MyPosts/MyPosts.jsx
@@ -2,11 +2,13 @@ import React from 'react';
 import c from './MyPosts.module.css'
 import Post from "./Post/Post";
 import {Field, reduxForm} from "redux-form";
+import {maxLength30, minLength2, required} from "../../../utils/validators/validators";
 
 let AddNewPostForm = (props) => {
     return <form onSubmit={props.handleSubmit}>
         <div>
-           <Field name = "newPostText" component="textarea"/>
+           <Field name = "newPostText" component="textarea"
+                  validate={[required, maxLength30, minLength2]}/>
         </div>
         <div>
             <button>Add post</button>
--- src/utils/validators/validators.js
+++ src/utils/validators/validators.js
@@ -1,5 +1,17 @@
-const requiredField = value => {
+export const required = value => {
     if (value) return undefined;
-    return "Field is required";
 
+    return "Field is required";
+}
+
+export const maxLength30 = value => {
+    if (value && value.length > 30) return "Max length is 30 symbols";
+
+    return undefined;
+}
+
+export const minLength2 = value => {
+    if (value && value.length < 2) return "Mix length is 2 symbols";
+
+    return undefined;
 }
