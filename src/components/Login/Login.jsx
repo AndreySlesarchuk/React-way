487ea606bda3435acc2f8b34d940ea0c20a5d5a7
Andrey Slesarchuk <andrey.slesarchuk@gmail.com>
2019-10-07 15:50:55 +0300

    validations Login

--- src/components/Login/Login.jsx
+++ src/components/Login/Login.jsx
@@ -1,17 +1,23 @@
 import React from 'react';
 import {Field, reduxForm} from "redux-form";
+import {Input} from "../common/FormsControls/FormsControls";
+import {required} from "../../utils/validators/validators";
 
 const LoginForm = (props) => {
     return (
         <form onSubmit={props.handleSubmit}>
             <div>
-                <Field placeholder={"Login"} name={"login"} component={"input"} />
+                <Field placeholder={"Login"} name={"login"}
+                       validate={[required]}
+                       component={Input} />
             </div>
             <div>
-                <Field placeholder={"Password"} name={"password"} component={"input"} />
+                <Field placeholder={"Password"} name={"password"}
+                       validate={[required]}
+                       component={Input} />
             </div>
             <div>
-                <Field name={"rememberMe"} type={"checkbox"} component={"input"} /> rememeber me
+                <Field name={"rememberMe"} type={"checkbox"} component={Input} /> rememeber me
             </div>
             <div>
                 <button>Login</button>
