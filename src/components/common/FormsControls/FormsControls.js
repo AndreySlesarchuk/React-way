9b1975f4076dfa8bbd60d587950f2d13d28392f5
Andrey Slesarchuk <andrey.slesarchuk@gmail.com>
2019-10-07 15:06:51 +0300

    Update FormsControls.js
    test styles.error

--- src/components/common/FormsControls/FormsControls.js
+++ src/components/common/FormsControls/FormsControls.js
@@ -1,9 +1,9 @@
 import React from "react";
-import styles from "./FormsControls.module.css"
+import styles from "./FormsControls.module.css";
 
 export const Textarea = ({input, meta, ...props}) => {
     return (
-        <div className={styles.formControl}>
+        <div className={styles.formControl + " " + styles.error}>
             <textarea {...input} {...props} />
         </div>
     )
