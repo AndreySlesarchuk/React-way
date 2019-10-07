fa453cec20ca8d4f455e956e4a8cc7af4ebd116c
Andrey Slesarchuk <andrey.slesarchuk@gmail.com>
2019-10-07 15:10:19 +0300

    added error span

--- src/components/common/FormsControls/FormsControls.js
+++ src/components/common/FormsControls/FormsControls.js
@@ -4,7 +4,10 @@ import styles from "./FormsControls.module.css";
 export const Textarea = ({input, meta, ...props}) => {
     return (
         <div className={styles.formControl + " " + styles.error}>
-            <textarea {...input} {...props} />
+            <div>
+                <textarea {...input} {...props} />
+            </div>
+            <span>{"some error"}</span>
         </div>
     )
 }
