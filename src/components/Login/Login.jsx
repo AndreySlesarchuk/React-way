9ff17c56000c79da732378fb747326d8e92a1c83
Andrey Slesarchuk <andrey.slesarchuk@gmail.com>
2019-10-24 17:24:47 +0300

    Update Login.jsx
    redirect to profile

--- src/components/Login/Login.jsx
+++ src/components/Login/Login.jsx
@@ -4,6 +4,7 @@ import {Input} from "../common/FormsControls/FormsControls";
 import {required} from "../../utils/validators/validators";
 import {connect} from "react-redux";
 import {login} from "../../redux/auth-reducer";
+import {Redirect} from "react-router-dom";
 
 const LoginForm = (props) => {
     return (
@@ -35,10 +36,19 @@ const Login = (props) => {
         props.login(formData.email, formData.password, formData.rememberMe);
         //console.log(formData);
     }
+
+    if (props.isAuth) {
+        return <Redirect to={"/profile"} />
+    }
+
     return <div>
         <h1>Login</h1>
         <LoginReduxForm onSubmit={onSubmit}/>
     </div>
 }
 
-export default connect(null, {login})(Login);
+const mapStateToProps = (state) => ({
+    isAuth: state.auth.isAuth
+})
+
+export default connect(mapStateToProps, {login})(Login);
