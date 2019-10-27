36cf9b1a178f1057e32d1bda6a851c76ee2d006c
Andrey Slesarchuk <andrey.slesarchuk@gmail.com>
2019-10-27 13:53:38 +0300

    app to class component

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, {Component} from 'react';
 import './App.css';
 import Navbar from "./components/Navbar/Navbar";
 import Todo from "./__test/Todo/Todo";
@@ -14,31 +14,33 @@ import ProfileContainer from "./components/Profile/ProfileContainer";
 import HeaderContainer from "./components/Header/HeaderContainer";
 import LoginPage from "./components/Login/Login";
 
-const App = () => {
+class App extends Component {
+    render() {
 
-    return (
-        <div className='app-wrapper'>
-            <HeaderContainer />
-            <Navbar/>
-            <div className='app-wrapper-content'>
-                <Route path='/profile/:userId?'
-                       render={() => <ProfileContainer /> }/>
-                <Route path='/dialogs'
-                       render={() => <DialogsContainer /> }/>
-                <Route path='/users'
-                       render={() => <UsersContainer />} />
-                <Route path='/login'
-                       render={() => <LoginPage />} />
-                <Route path='/music' render={() => <Music />} />
-                <Route path='/news' render={() => <News />} />
-                <Route path='/todo' render={() => <Todo />} />
-                <Route path='/meme' render={() => <Meme />} />
-                <Route path='/map' render={() => <MyMap />} />
-                <Route path='/geomap' render={() => <Geomap style={{height: '1000px'}} view={[53.9, 27.5]}/>} />
-                <Route path='/settings' render={() => <Todo />} />
+        return (
+            <div className='app-wrapper'>
+                <HeaderContainer/>
+                <Navbar/>
+                <div className='app-wrapper-content'>
+                    <Route path='/profile/:userId?'
+                           render={() => <ProfileContainer/>}/>
+                    <Route path='/dialogs'
+                           render={() => <DialogsContainer/>}/>
+                    <Route path='/users'
+                           render={() => <UsersContainer/>}/>
+                    <Route path='/login'
+                           render={() => <LoginPage/>}/>
+                    <Route path='/music' render={() => <Music/>}/>
+                    <Route path='/news' render={() => <News/>}/>
+                    <Route path='/todo' render={() => <Todo/>}/>
+                    <Route path='/meme' render={() => <Meme/>}/>
+                    <Route path='/map' render={() => <MyMap/>}/>
+                    <Route path='/geomap' render={() => <Geomap style={{height: '1000px'}} view={[53.9, 27.5]}/>}/>
+                    <Route path='/settings' render={() => <Todo/>}/>
+                </div>
             </div>
-        </div>
-    );
+        );
+    }
 }
 
 export default App;
