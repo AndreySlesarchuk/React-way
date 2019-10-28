285dc0591d916d6e2206cce66b4188afb8dd9d83
Andrey Slesarchuk <andrey.slesarchuk@gmail.com>
2019-10-28 15:55:52 +0300

    Update app-reducer.js

--- src/redux/app-reducer.js
+++ src/redux/app-reducer.js
@@ -21,36 +21,8 @@ const appReducer = (state = initialState, action) => {
 
 export const initializedSuccess = () => ({ type: INITIALIZED_SUCCESS });
 
-//thunk creator - function that returns another function - dispatch
-export const getAuthUserData = () => (dispatch) => {
-    authAPI.me()
-        .then(response => {
-            if  (response.data.resultCode === 0) {
-                let {id, email, login} = response.data.data;
-                dispatch(setAuthUserData(id, email, login, true));
-            }
-        });
-}
+export const initialize = () => (dispatch) => {
 
-export const login = (email, password, rememberMe) => (dispatch) => {
-    authAPI.login(email, password, rememberMe)
-        .then(response => {
-            if  (response.data.resultCode === 0) {
-                dispatch(getAuthUserData())
-            } else {
-                let message = response.data.messages.length > 0 ? response.data.messages[0] : "Some error"
-                dispatch(stopSubmit("login", {_error: message}));
-            }
-        });
-}
-
-export const logout = () => (dispatch) => {
-    authAPI.logout()
-        .then(response => {
-            if  (response.data.resultCode === 0) {
-                dispatch(setAuthUserData(null, null, null, false))
-            }
-        });
 }
 
 export default appReducer;
