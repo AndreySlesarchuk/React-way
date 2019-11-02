74f6d3214468f675bf3e48aba6739dc47b9d12e7
Andrey Slesarchuk <andrey.slesarchuk@gmail.com>
2019-11-02 18:44:55 +0300

    Update ProfileStatusWithHooks.jsx

--- src/components/Profile/ProfileInfo/ProfileStatusWithHooks.jsx
+++ src/components/Profile/ProfileInfo/ProfileStatusWithHooks.jsx
@@ -3,12 +3,12 @@ import React from 'react';
 const ProfileStatusWithHooks = (props) => {
         return (
             <div>
-                {!this.state.editMode &&
+                {true &&
                 <div>
                     <span onDoubleClick={this.activateEditMode}>{this.props.status || "----------"}</span>
                 </div>
                 }
-                {this.state.editMode &&
+                {false &&
                 <div>
                     <input onChange={this.onStatusChange}
                         autoFocus={true} onBlur={this.deactivateEditMode} value={this.state.status}/>
