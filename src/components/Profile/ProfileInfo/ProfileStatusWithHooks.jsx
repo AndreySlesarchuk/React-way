7826804fc0311bcebc1f3d3bf145f50b63fbb8e5
Andrey Slesarchuk <andrey.slesarchuk@gmail.com>
2019-11-02 19:20:32 +0300

    Update ProfileStatusWithHooks.jsx

--- src/components/Profile/ProfileInfo/ProfileStatusWithHooks.jsx
+++ src/components/Profile/ProfileInfo/ProfileStatusWithHooks.jsx
@@ -2,15 +2,17 @@ import React, {useState} from 'react';
 
 const ProfileStatusWithHooks = (props) => {
 
-        let stateWithSetState = useState(false);
-        let editMode = stateWithSetState[0];
-        let setEditMode = stateWithSetState[1];
+        let [editMode, setEditMode] = useState(false);
+
+        const activateEditMode = () => {
+            setEditMode(true);
+        }
 
         return (
             <div>
                 { !editMode &&
                 <div>
-                    <span>{props.status || "------------"}</span>
+                    <span onDoubleClick={activateEditMode}>{props.status || "------------"}</span>
                 </div>
                 }
                 { editMode &&
