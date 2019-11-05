b1db56400348ac3c5ef3585a8ca39379b2493085
Andrey Slesarchuk <andrey.slesarchuk@gmail.com>
2019-11-05 22:10:20 +0300

    Update ProfileInfo.jsx

--- src/components/Profile/ProfileInfo/ProfileInfo.jsx
+++ src/components/Profile/ProfileInfo/ProfileInfo.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 import c from './ProfileInfo.module.css'
 import Preloader from "../../common/Preloader/Preloader";
 import ProfileStatusWithHooks from "./ProfileStatusWithHooks";
-//import backgroundImage from "../../../assets/images/bg_cells.jpg";
+import backgroundImage from "../../../assets/images/bg_cells.jpg";
 //<img src='https://cdnb.artstation.com/p/assets/images/images/005/754/755/large/jayson-kassis-highresscreenshot00199.jpg?1493552247'/>
 //<img src='https://cdn-images-1.medium.com/max/1600/1*CvjclMDwgh9AqED_h5UVCA.jpeg'/>
 
@@ -12,9 +12,9 @@ const ProfileInfo = (props) => {
     }
     return (
         <div>
-            {/*<div className={c.content}>
+            {<div className={c.content}>
                 <img src={backgroundImage}/>
-            </div>*/}
+            </div>}
             <div className={c.descriptionBlock}>
                 <img src={props.profile.photos.large} alt="" />
                 <ProfileStatusWithHooks status={props.status} updateStatus={props.updateStatus} />
