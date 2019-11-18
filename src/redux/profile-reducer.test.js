99557388cb8f0bdeb84e53668e088a70d0e85832
Andrey Slesarchuk <andrey.slesarchuk@gmail.com>
2019-11-18 15:56:16 +0300

    Update profile-reducer.test.js

--- src/redux/profile-reducer.test.js
+++ src/redux/profile-reducer.test.js
@@ -1,8 +1,18 @@
-import profileReducer from "./profile-reducer";
+import profileReducer, {addPostActionCreator} from "./profile-reducer";
 import React from 'react';
 
 it('new post should be added', () => {
-    let newState = profileReducer({},{});
+    let action = addPostActionCreator("it-kamasutra.com");
+    let state = {
+        posts: [
+            {id: 1, message: 'Hello!', likesCount: 13},
+            {id: 2, message: 'Good day', likesCount: 8},
+            {id: 3, message: 'Victory day', likesCount: 7},
+            {id: 4, message: 'summer evening', likesCount: 11}
+        ]
+    };
+
+    let newState = profileReducer(state, action);
 });
 
 
