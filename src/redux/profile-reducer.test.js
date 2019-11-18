0d329a29630b42fb8c86e1c31180a0db1f49ab25
Andrey Slesarchuk <andrey.slesarchuk@gmail.com>
2019-11-18 16:07:36 +0300

    Update profile-reducer.test.js

--- src/redux/profile-reducer.test.js
+++ src/redux/profile-reducer.test.js
@@ -1,22 +1,32 @@
 import profileReducer, {addPostActionCreator} from "./profile-reducer";
 import React from 'react';
 
+let state = {
+    posts: [
+        {id: 1, message: 'Hello!', likesCount: 13},
+        {id: 2, message: 'Good day', likesCount: 8},
+        {id: 3, message: 'Victory day', likesCount: 7},
+        {id: 4, message: 'summer evening', likesCount: 11}
+    ]
+};
+
 it('length of post should be incremented', () => {
     //1. test data
     let action = addPostActionCreator("it-kamasutra.com");
-    let state = {
-        posts: [
-            {id: 1, message: 'Hello!', likesCount: 13},
-            {id: 2, message: 'Good day', likesCount: 8},
-            {id: 3, message: 'Victory day', likesCount: 7},
-            {id: 4, message: 'summer evening', likesCount: 11}
-        ]
-    };
     //2. action
     let newState = profileReducer(state, action);
-
     //3. expectation
     expect(newState.posts.length).toBe(5);
+
+});
+
+it('message of new post should be "it-kamasutra.com"', () => {
+    //1. test data
+    let action = addPostActionCreator("it-kamasutra.com");
+    //2. action
+    let newState = profileReducer(state, action);
+    //3. expectation
+    expect(newState.posts[4].message).toBe("it-kamasutra.com");
 });
 
 
