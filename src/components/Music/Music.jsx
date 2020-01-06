216e2b78e9685ab8b9248062ee8b044f45b4086a
Andrey Slesarchuk <andrey.slesarchuk@gmail.com>
2020-01-06 21:53:23 +0300

    Update Music.jsx

--- src/components/Music/Music.jsx
+++ src/components/Music/Music.jsx
@@ -6,18 +6,19 @@ const  Music = () => {
         <div className={c.music}>
             <div className={c.musicAlbums}>
                 <div className={c.album}> UDO </div>
+                <div className={c.album}> The Technology </div>
                 <div className={c.album}> Accept </div>
                 <div className={c.album}> KINO </div>
                 <div className={c.album}> Accept </div>
-                <div className={c.album}> The Technology </div>
+
                 <div className={c.album}> Nirvana </div>
             </div>
             <div className={c.songs}>
                 <div className={c.song}>Crying soldier</div>
+                <div className={c.song}>Click on the button</div>
                 <div className={c.song}>Shadow soldier's </div>
                 <div className={c.song}>April</div>
                 <div className={c.song}>Fall of the Empire</div>
-                <div className={c.song}>Click on the button</div>
                 <div className={c.song}>Rape me</div>
             </div>
         </div>
