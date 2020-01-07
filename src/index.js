1857ff44bc0dcd3b3302083603f98628879ffc48
Andrey Slesarchuk <andrey.slesarchuk@gmail.com>
2020-01-07 22:30:56 +0300

    Update index.js

--- src/index.js
+++ src/index.js
@@ -9,7 +9,6 @@ import {Provider} from "react-redux";
 //let h1 = document.createElement("h1");
 //h1.innerHTML = "Hello";
 //document.querySelector("body").appendChild(h1);
-
 /*setInterval(()=>{ store.dispatch({type: "FAKE"})}, 1000);*/
 
 ReactDOM.render(
