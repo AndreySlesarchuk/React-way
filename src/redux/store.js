b55fa91cd76c6601d11e3bb24ab72eda047ec2a5
Andrey Slesarchuk <andrey.slesarchuk@gmail.com>
2020-01-13 22:47:33 +0300

    Update store.js

--- src/redux/store.js
+++ src/redux/store.js
@@ -45,7 +45,7 @@ let store = {
         sidebar: {}
     },
     _callSubscriber() {
-        console.log('State was changed');
+        console.log('State was changed.');
     },
 
     getState() {
@@ -60,9 +60,7 @@ let store = {
         this._state.profilePage = profileReducer(this._state.profilePage, action);
         this._state.dialogsPage = dialogsReducer(this._state.dialogsPage, action);
         this._state.sidebar = sidebarReducer(this._state.sidebar, action);
-
         this._callSubscriber(this._state); // notified the subscriber | уведомили подписчика
-
     }
 }
 
