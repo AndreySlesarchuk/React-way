e69e244d3acb5017d5183e7fc582ad0130bdbf7a
Andrey Slesarchuk <andrey.slesarchuk@gmail.com>
2020-01-14 19:05:03 +0300

    Update store.js

--- src/redux/store.js
+++ src/redux/store.js
@@ -23,7 +23,7 @@ let store = {
                 {id: 4, name: 'Andrey'},
                 {id: 5, name: 'Vasiliy'},
                 {id: 6, name: 'Natasha'},
-                {id: 7, name: 'Sergey'}
+                {id: 7, name: 'Jon'}
 
             ],
             messages: [
