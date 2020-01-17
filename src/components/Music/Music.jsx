84626422eef61e35e083f2f168ba61aa5b822192
Andrey Slesarchuk <andrey.slesarchuk@gmail.com>
2020-01-17 23:03:02 +0300

    Update Music.jsx

--- src/components/Music/Music.jsx
+++ src/components/Music/Music.jsx
@@ -7,7 +7,6 @@ const  Music = () => {
             <div className={c.musicAlbums}>
                 <div className={c.album}> The Technology </div>
                 <div className={c.album}> UDO </div>
-
                 <div className={c.album}> Accept </div>
                 <div className={c.album}> KINO </div>
                 <div className={c.album}> Accept </div>
@@ -16,7 +15,6 @@ const  Music = () => {
             <div className={c.songs}>
                 <div className={c.song}>Click on the button</div>
                 <div className={c.song}>Crying soldier</div>
-
                 <div className={c.song}>Shadow soldier's </div>
                 <div className={c.song}>April</div>
                 <div className={c.song}>Fall of the Empire</div>
