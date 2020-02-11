9939b5c3716895eb4ac04af1da8a26f64af3d05c
Andrey Slesarchuk <andrey.slesarchuk@gmail.com>
2020-02-11 21:40:15 +0300

    Update Music.jsx

--- src/components/Music/Music.jsx
+++ src/components/Music/Music.jsx
@@ -5,18 +5,20 @@ const  Music = () => {
     return (
         <div className={c.music}>
             <div className={c.musicAlbums}>
+                <div className={c.album}> KINO</div>
                 <div className={c.album}> The Technology </div>
                 <div className={c.album}> Accept</div>
-                <div className={c.album}> KINO</div>
+
                 <div className={c.album}> Didier Marouani & SPACE</div>
                 <div className={c.album}> Accept</div>
                 <div className={c.album}> Nirvana </div>
                 <div className={c.album}> UDO </div>
             </div>
             <div className={c.songs}>
+                <div className={c.song}>April</div>
                 <div className={c.song}>Strange dances</div>
                 <div className={c.song}>Fall of the Empire</div>
-                <div className={c.song}>April</div>
+
                 <div className={c.song}>From Earth to Mars</div>
                 <div className={c.song}>Shadow soldier's</div>
                 <div className={c.song}>Spanish caravan</div>
