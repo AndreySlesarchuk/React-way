46f049d48d52315f9296c862a97a251b15e52e3d
Andrey Slesarchuk <andrey.slesarchuk@gmail.com>
2020-03-10 23:08:04 +0300

    Update api.js

--- src/api/api.js
+++ src/api/api.js
@@ -11,15 +11,15 @@ const instance = axios.create({
 export const usersAPI = {
     getUsers(currentPage = 1, pageSize = 17) {
         return instance.get(`users?page=${currentPage}&count=${pageSize}`)
-            .then(response => response.data);
+            .then(response => response.data)
     },
     follow(userId) {
         return instance.post(`follow/${userId}`)
-            .then(response => response.data);
+            .then(response => response.data)
     },
     unfollow(userId) {
         return instance.delete(`follow/${userId}`)
-            .then(response => response.data);
+            .then(response => response.data)
     }
 }
 
