217d963c05065e3163c7e0a13bcee48b770c497f
Andrey Slesarchuk <andrey.slesarchuk@gmail.com>
2020-03-11 11:19:03 +0300

    Update users-reducer.js

--- src/redux/users-reducer.js
+++ src/redux/users-reducer.js
@@ -10,7 +10,7 @@ const TOGGLE_IS_FOLLOWING_PROGRESS = 'TOGGLE_IS_FOLLOWING_PROGRESS'
 
 let initialState = {
     users: [],
-    pageSize: 20,
+    pageSize: 21,
     totalUsersCount: 0,
     currentPage: 1,
     isFetching: false, // is getting
