3658f59d3ba20d6ed2a293db267ca1433921dcf5
Andrey Slesarchuk <andrey.slesarchuk@gmail.com>
2020-03-17 22:58:28 +0300

    Update index.js

--- src/index.js
+++ src/index.js
@@ -3,8 +3,8 @@ import * as serviceWorker from './serviceWorker'
 import ReactDOM from "react-dom"
 import App from "./App"
 import {BrowserRouter} from "react-router-dom"
-import store from "./redux/redux-store";
-import {Provider} from "react-redux";
+import store from "./redux/redux-store"
+import {Provider} from "react-redux"
 
 //let h1 = document.createElement("h1");
 //h1.innerHTML = "Hello";
