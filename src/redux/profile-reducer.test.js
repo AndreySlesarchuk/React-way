0fe73285a45f4be1fb0799d8d12501e0d51289de
Andrey Slesarchuk <andrey.slesarchuk@gmail.com>
2020-03-21 17:08:10 +0300

    Update profile-reducer.test.js

--- src/redux/profile-reducer.test.js
+++ src/redux/profile-reducer.test.js
@@ -31,11 +31,11 @@ it('message of new post should be "it-kamasutra.com"', () => {
 
 it('after deleting length of messages should be decrement', () => {
     //1. test data
-    let action = deletePost(1);
+    let action = deletePost(1)
     //2. action
-    let newState = profileReducer(state, action);
+    let newState = profileReducer(state, action)
     //3. expectation
-    expect(newState.posts.length).toBe(3);
+    expect(newState.posts.length).toBe(3)
 });
 
 it(`after deleting length shouldn't be decrement if id is incorrect`, () => {
