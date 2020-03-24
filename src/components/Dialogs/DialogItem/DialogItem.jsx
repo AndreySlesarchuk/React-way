4f5a6fc2a0128af22a07f9540a4e4ffe3024da54
Andrey Slesarchuk <andrey.slesarchuk@gmail.com>
2020-03-24 22:00:20 +0300

    Update DialogItem.jsx

--- src/components/Dialogs/DialogItem/DialogItem.jsx
+++ src/components/Dialogs/DialogItem/DialogItem.jsx
@@ -1,5 +1,5 @@
 import React from 'react'
-import c from './../Dialogs.module.css';
+import c from './../Dialogs.module.css'
 import {NavLink} from "react-router-dom";
 
 const DialogItem = (props) => {
