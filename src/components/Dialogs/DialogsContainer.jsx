305dc0113e22d3bea320161bd13249cd7526ef32
Andrey Slesarchuk <andrey.slesarchuk@gmail.com>
2020-03-25 22:28:30 +0300

    Update DialogsContainer.jsx

--- src/components/Dialogs/DialogsContainer.jsx
+++ src/components/Dialogs/DialogsContainer.jsx
@@ -1,8 +1,8 @@
-import {sendMessageCreator, updateNewMessageBodyCreator} from "../../redux/dialogs-reducer";
-import Dialogs from "./Dialogs";
-import {connect} from "react-redux";
-import {withAuthRedirect} from "../../hoc/withAuthRedirect";
-import {compose} from "redux";
+import {sendMessageCreator, updateNewMessageBodyCreator} from "../../redux/dialogs-reducer"
+import Dialogs from "./Dialogs"
+import {connect} from "react-redux"
+import {withAuthRedirect} from "../../hoc/withAuthRedirect"
+import {compose} from "redux"
 
 let mapStateToProps = (state) => {
     return {
