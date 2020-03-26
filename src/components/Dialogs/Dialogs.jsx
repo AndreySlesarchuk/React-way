734661fa69aee6e94146264fd343c7b7d83e386f
Andrey Slesarchuk <andrey.slesarchuk@gmail.com>
2020-03-26 22:37:55 +0300

    Update Dialogs.jsx

--- src/components/Dialogs/Dialogs.jsx
+++ src/components/Dialogs/Dialogs.jsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React from "react"
 import c from './Dialogs.module.css';
 import DialogItem from "./DialogItem/DialogItem";
 import Message from "./Message/Message";
