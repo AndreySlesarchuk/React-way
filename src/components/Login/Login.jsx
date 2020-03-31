8fd81497c16a48b26ae8b4af9d0c6790dfb8ef85
Andrey Slesarchuk <andrey.slesarchuk@gmail.com>
2020-03-31 22:13:12 +0300

    Update Login.jsx

--- src/components/Login/Login.jsx
+++ src/components/Login/Login.jsx
@@ -1,5 +1,5 @@
 import React from 'react'
-import {Field, reduxForm} from "redux-form";
+import {Field, reduxForm} from "redux-form"
 import {Input} from "../common/FormsControls/FormsControls";
 import {required} from "../../utils/validators/validators";
 import {connect} from "react-redux";
