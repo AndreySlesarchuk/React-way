da97c2ab31324923c577c47860830a0144d972ba
Andrey Slesarchuk <andrey.slesarchuk@gmail.com>
2020-04-12 21:48:40 +0300

    Update ProfileContainer.jsx

--- src/components/Profile/ProfileContainer.jsx
+++ src/components/Profile/ProfileContainer.jsx
@@ -44,4 +44,4 @@ let mapStateToProps = (state) => {
 export default compose(
     connect(mapStateToProps, {getUserProfile, getStatus, updateStatus}),
     withRouter, //withAuthRedirect
-)(ProfileContainer);
+)(ProfileContainer)
