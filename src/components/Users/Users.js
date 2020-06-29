b97d2ce8aaccf3d49c9aa2f0335e84a9ed3f3274
Andrey Slesarchuk <andrey.slesarchuk@gmail.com>
2020-06-29 21:53:01 +0300

    Update Users.js

--- src/components/Users/Users.js
+++ src/components/Users/Users.js
@@ -8,7 +8,9 @@ let Users = (props) => {
     let pagesCount = Math.ceil(props.totalUsersCount / props.pageSize);
 
     let pages = [];
-    for (let i = 1; i <= pagesCount; i++) { pages.push(i) }
+    for (let i = 1; i <= pagesCount; i++) {
+        pages.push(i)
+    }
 
     return <div>
         <div>
@@ -25,15 +27,19 @@ let Users = (props) => {
                     <div>
                         <NavLink to={'/profile/' + u.id}>
                             <img src={u.photos.small != null ? u.photos.small : userPhoto}
-                                 className={styles.userPhoto} alt="" />
+                                 className={styles.userPhoto} alt=""/>
                         </NavLink>
                     </div>
                     <div>
                         {u.followed
                             ? <button disabled={props.followingInProgress.some(id => id === u.id)}
-                                      onClick={() => {props.unfollow(u.id);}}>Unfollow</button>
+                                      onClick={() => {
+                                          props.unfollow(u.id);
+                                      }}>Unfollow</button>
                             : <button disabled={props.followingInProgress.some(id => id === u.id)}
-                                      onClick={() => {props.follow(u.id);}}>Follow</button>}
+                                      onClick={() => {
+                                          props.follow(u.id);
+                                      }}>Follow</button>}
                     </div>
                 </span>
                 <span>
