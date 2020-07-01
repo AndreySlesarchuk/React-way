87c7d5b3ae066ac94734a7d5d0552dc5157890aa
Andrey Slesarchuk <andrey.slesarchuk@gmail.com>
2020-07-01 22:49:49 +0300

    Update App.js

--- src/App.js
+++ src/App.js
@@ -5,7 +5,7 @@ import Todo from './__test/Todo/Todo'
 import Meme from './__test/Meme/Meme'
 import {Route, withRouter} from 'react-router-dom'
 import Music from './components/Music/Music'
-import News from "./components/News/News"
+import News from './components/News/News'
 import Geomap from "./components/Geomaps/Geomap"
 import MyMap from "./components/Geomaps/MyMap"
 import DialogsContainer from "./components/Dialogs/DialogsContainer"
