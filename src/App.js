b45b086b9fcd927e073995c17ec5cb5dd4c855b3
Andrey Slesarchuk <andrey.slesarchuk@gmail.com>
2020-07-02 22:23:52 +0300

    Update App.js

--- src/App.js
+++ src/App.js
@@ -9,7 +9,7 @@ import News from './components/News/News'
 import Geomap from './components/Geomaps/Geomap'
 import MyMap from './components/Geomaps/MyMap'
 import DialogsContainer from './components/Dialogs/DialogsContainer'
-import UsersContainer from "./components/Users/UsersContainer"
+import UsersContainer from './components/Users/UsersContainer'
 import ProfileContainer from "./components/Profile/ProfileContainer"
 import HeaderContainer from "./components/Header/HeaderContainer"
 import LoginPage from "./components/Login/Login"
