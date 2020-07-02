8c9e5e431d3fe4d2f7731fe1c9f4d8428d57fbbc
Andrey Slesarchuk <andrey.slesarchuk@gmail.com>
2020-07-02 22:23:01 +0300

    Update App.js

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ import {Route, withRouter} from 'react-router-dom'
 import Music from './components/Music/Music'
 import News from './components/News/News'
 import Geomap from './components/Geomaps/Geomap'
-import MyMap from "./components/Geomaps/MyMap"
+import MyMap from './components/Geomaps/MyMap'
 import DialogsContainer from './components/Dialogs/DialogsContainer'
 import UsersContainer from "./components/Users/UsersContainer"
 import ProfileContainer from "./components/Profile/ProfileContainer"
