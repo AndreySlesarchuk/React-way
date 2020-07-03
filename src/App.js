d5f634bde2d0bcd70ef162f1cba7d479098c0e17
Andrey Slesarchuk <andrey.slesarchuk@gmail.com>
2020-07-03 22:00:04 +0300

    Update App.js

--- src/App.js
+++ src/App.js
@@ -13,7 +13,7 @@ import UsersContainer from './components/Users/UsersContainer'
 import ProfileContainer from './components/Profile/ProfileContainer'
 import HeaderContainer from './components/Header/HeaderContainer'
 import LoginPage from './components/Login/Login'
-import {connect} from "react-redux"
+import {connect} from 'react-redux'
 import {compose} from "redux"
 import {initializeApp} from "./redux/app-reducer"
 import Preloader from "./components/common/Preloader/Preloader"
