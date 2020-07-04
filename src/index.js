d7776372b8b2c4d889b8c1bab84bc460451c1687
Andrey Slesarchuk <andrey.slesarchuk@gmail.com>
2020-07-04 21:37:25 +0300

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import React from 'react'
 import * as serviceWorker from './serviceWorker'
 import ReactDOM from 'react-dom'
-import App from "./App"
+import App from './App'
 import {BrowserRouter} from "react-router-dom"
 import store from "./redux/redux-store"
 import {Provider} from "react-redux"
