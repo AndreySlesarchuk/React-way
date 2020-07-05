e6f3135f72568a789664ce2967a5fb4398adab95
Andrey Slesarchuk <andrey.slesarchuk@gmail.com>
2020-07-05 21:05:25 +0300

    Update Preloader.js

--- src/components/common/Preloader/Preloader.js
+++ src/components/common/Preloader/Preloader.js
@@ -1,5 +1,5 @@
 import React from 'react'
-import preloader from "../../../assets/images/Pacman-1s-200px.svg"
+import preloader from '../../../assets/images/Pacman-1s-200px.svg'
 
 let Preloader = () => {
     return <div>
