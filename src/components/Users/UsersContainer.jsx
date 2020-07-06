e5732d65e65394b1ec3d0f35825bd162caf974f4
Andrey Slesarchuk <andrey.slesarchuk@gmail.com>
2020-07-06 22:50:16 +0300

    Update UsersContainer.jsx

--- src/components/Users/UsersContainer.jsx
+++ src/components/Users/UsersContainer.jsx
@@ -1,5 +1,5 @@
 import React from 'react'
-import {connect} from "react-redux"
+import {connect} from react-redux
 import {follow, requestUsers, setCurrentPage, toggleFollowingProgress, unfollow} from "../../redux/users-reducer"
 import Users from './Users'
 import Preloader from "../common/Preloader/Preloader"
