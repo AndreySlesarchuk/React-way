b40bc76fb4b86840df276e9ebc8461f598edc280
Andrey Slesarchuk <andrey.slesarchuk@gmail.com>
2020-07-10 22:32:52 +0300

    Update store.js

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,5 +1,5 @@
 import profileReducerOld from './profile-reducer'
-import dialogsReducer from "./dialogs-reducer"
+import dialogsReducer from './dialogs-reducer'
 import sidebarReducer from "./sidebar-reducer"
 
 let store = {
