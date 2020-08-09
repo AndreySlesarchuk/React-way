da8c5ea32377a0fe027f7baf94d44e5a584fce7c
Andrey Slesarchuk <andrey.slesarchuk@gmail.com>
2020-08-09 21:24:06 +0300

    Update Music.jsx

--- src/components/Music/Music.jsx
+++ src/components/Music/Music.jsx
@@ -21,7 +21,6 @@ const  Music = () => {
                 <div className={c.song}>Shadow soldier's</div>
                 <div className={c.song}>Strange dances</div>
                 <div className={c.song}>Fall of the Empire</div>
-
             </div>
         </div>
     )
