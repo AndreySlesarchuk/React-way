9bfbcce1321a2418f2845f71fb0de4da082c05a0
Andrey Slesarchuk <andrey.slesarchuk@gmail.com>
2020-08-13 21:09:33 +0300

    Update Music.jsx

--- src/components/Music/Music.jsx
+++ src/components/Music/Music.jsx
@@ -5,8 +5,9 @@ const  Music = () => {
     return (
         <div className={c.music}>
             <div className={c.musicAlbums}>
-                <div className={c.album}>UDO</div>
                 <div className={c.album}>Accept</div>
+                <div className={c.album}>UDO</div>
+
                 <div className={c.album}>Accept</div>
                 <div className={c.album}>KINO</div>
                 <div className={c.album}>Didier Marouani & SPACE</div>
