276a5fa2848a03cdf2bd5214a3743eac2a7ce0f2
Andrey Slesarchuk <andrey.slesarchuk@gmail.com>
2020-09-01 21:48:46 +0300

    Update auth-reducer.js

--- src/redux/auth-reducer.js
+++ src/redux/auth-reducer.js
@@ -23,12 +23,12 @@ const authReducer = (state = initialState, action) => {
 }
 
 export const setAuthUserData = (userId, email, login, isAuth) => (
-    { type: SET_USER_DATA, payload: {userId, email, login, isAuth} })
+    {type: SET_USER_DATA, payload: {userId, email, login, isAuth}})
 
 //thunk creator - function that returns another function - dispatch//
 export const getAuthUserData = () => async (dispatch) => {
     let response = await authAPI.me();
-    if  (response.data.resultCode === 0) {
+    if (response.data.resultCode === 0) {
         let {id, email, login} = response.data.data;
         dispatch(setAuthUserData(id, email, login, true))
     }
