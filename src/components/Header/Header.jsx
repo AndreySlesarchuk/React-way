9052ffb37aa22c49ac3d4df0172159b7d0d13415
Andrey Slesarchuk <andrey.slesarchuk@gmail.com>
2020-09-01 21:51:01 +0300

    Update Header.jsx

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -3,12 +3,14 @@ import style from './Header.module.css'
 import {NavLink} from "react-router-dom"
 
 const Header = (props) => {
-    return  <header className={style.header}>
-        <img className='App-logo' src="http://192.168.0.202/623/custom/themes/default/images/company_logo.png?v=NyHHMP7OnQElMG2V_-oPog" alt=''/>
+    return <header className={style.header}>
+        <img className='App-logo'
+             src="http://192.168.0.202/623/custom/themes/default/images/company_logo.png?v=NyHHMP7OnQElMG2V_-oPog"
+             alt=''/>
         <div className={style.loginBlock}>
-            { props.isAuth
+            {props.isAuth
                 ? <div>{props.login} - <button onClick={props.logout}>Logout</button></div>
-                : <NavLink to={'/login'}>Login</NavLink> }
+                : <NavLink to={'/login'}>Login</NavLink>}
         </div>
     </header>
 }
