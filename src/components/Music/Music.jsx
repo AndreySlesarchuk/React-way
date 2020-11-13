98e81cdab88ab0716df480bd0f68fc164bed3c0b
Andrey Slesarchuk <andrey.slesarchuk@gmail.com>
2020-11-13 21:54:52 +0300

    Update Music.jsx

--- src/components/Music/Music.jsx
+++ src/components/Music/Music.jsx
@@ -10,7 +10,6 @@ const  Music = () => {
                 <div className={c.album}>OTTA Orchestra</div>
                 <div className={c.album}>Splin</div>
                 <div className={c.album}>Nautilus Pompilius</div>
-
                 <div className={c.album}>Splin</div>
                 <div className={c.album}>Accept</div>
                 <div className={c.album}>KINO</div>
@@ -26,7 +25,6 @@ const  Music = () => {
                 <div className={c.album}>The beast</div>
                 <div className={c.song}>Crying soldier</div>
                 <div className={c.album}>Church</div>
-
                 <div className={c.song}>April</div>
                 <div className={c.song}>From Earth to Mars</div>
                 <div className={c.song}>Strange dances</div>
