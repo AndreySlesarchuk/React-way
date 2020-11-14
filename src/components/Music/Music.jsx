90398af7e126ae437bb74dec5c0f6ca44aa87c76
Andrey Slesarchuk <andrey.slesarchuk@gmail.com>
2020-11-14 21:24:51 +0300

    Update Music.jsx

--- src/components/Music/Music.jsx
+++ src/components/Music/Music.jsx
@@ -12,7 +12,6 @@ const  Music = () => {
                 <div className={c.album}>Nautilus Pompilius</div>
                 <div className={c.album}>Accept</div>
                 <div className={c.album}>Splin</div>
-
                 <div className={c.album}>KINO</div>
                 <div className={c.album}>Didier Marouani & SPACE</div>
                 <div className={c.album}>The Technology </div>
