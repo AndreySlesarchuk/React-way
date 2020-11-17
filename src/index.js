0a8f3981782983af041a151803e0fc9a5afa633c
Andrey Slesarchuk <andrey.slesarchuk@gmail.com>
2020-11-17 22:15:19 +0300

    Update index.js

--- src/index.js
+++ src/index.js
@@ -6,12 +6,12 @@ import {BrowserRouter} from "react-router-dom"
 import store from './redux/redux-store'
 import {Provider} from 'react-redux'
 
-/*
+/*-
 let h1 = document.createElement("h1");
 h1.innerHTML = "Hello";
 document.querySelector("body").appendChild(h1);
 setInterval(()=>{ store.dispatch({type: "FAKE"})}, 1000);
- */
+-*/
 
 ReactDOM.render(
     <BrowserRouter>
