9842f0f6187a242356e46ffebe9a894c8f985884
Andrey Slesarchuk <andrey.slesarchuk@gmail.com>
2020-11-27 21:56:36 +0300

    Update Music.jsx

--- src/components/Music/Music.jsx
+++ src/components/Music/Music.jsx
@@ -20,6 +20,7 @@ const  Music = () => {
                 <div className={c.album}>Nirvana</div>
             </div>
             <div className={c.songs}>
+                <div className={c.song}>From Earth to Mars</div>
                 <div className={c.song}>Push the button</div>
                 <div className={c.song}>Strange dances</div>
                 <div className={c.song}>Fall of the Empire</div>
@@ -30,7 +31,7 @@ const  Music = () => {
                 <div className={c.album}>The beast</div>
                 <div className={c.album}>Church</div>
                 <div className={c.song}>April</div>
-                <div className={c.song}>From Earth to Mars</div>
+
                 <div className={c.song}>Spanish caravan</div>
             </div>
         </div>
