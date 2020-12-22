edede8285a9419df352561d9b73b558894ebf79a
Andrey Slesarchuk <andrey.slesarchuk@gmail.com>
2020-12-22 20:20:33 +0300

    Update UsersContainer.jsx

--- src/components/Users/UsersContainer.jsx
+++ src/components/Users/UsersContainer.jsx
@@ -10,7 +10,7 @@ import {
     getIsFetching,
     getPageSize,
     getTotalUsersCount,
-    getUsers, getUsersSuperSelector
+    getUsers
 } from '../../redux/users-selectors'
 
 class UsersContainer extends React.Component {
